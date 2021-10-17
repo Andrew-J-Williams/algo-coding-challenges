14726848186ff91b50bb2518e2bed149e4e15065
Andrew Williams <1williamsaj@gmail.com>
2021-10-17 10:16:16 -0500

    Auto formatted code in ValidateSubsequence

--- src/com/challenges/easy/ValidateSubsequence.java
+++ src/com/challenges/easy/ValidateSubsequence.java
@@ -1,6 +1,13 @@
 package com.challenges.easy;
 
+import java.util.List;
+
 public class ValidateSubsequence {
+
+	public static boolean isValidSubsequence(List<Integer> array, List<Integer> sequence) {
+		// Write your code here.
+		return false;
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub

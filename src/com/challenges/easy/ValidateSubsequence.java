af9e02fe9845c5331aaeacc10cdaa6ceb0cca055
Andrew Williams <1williamsaj@gmail.com>
2021-10-18 12:51:53 -0500

    Added comments to explain the logic of the solution for ValidateSubsequence

--- src/com/challenges/easy/ValidateSubsequence.java
+++ src/com/challenges/easy/ValidateSubsequence.java
@@ -27,20 +27,30 @@ public class ValidateSubsequence {
 
 	public static boolean isValidSubsequence(List<Integer> array, List<Integer> sequence) {
 		
+		// 1. We first use a 'for' loop to iterate over the elements in the main List, where 'x' represents an integer from the list.
 		for(Integer x : array) {
-					
+			
+			// 2. We check if this value is equal to the first value of the subsequence. 
 			if(x == sequence.get(0)) {
+				
+				// 3. If it is, we remove the first element, which automatically shifts subsequent elements (if they exist) to the left and decreases their indexes by 1.
 				sequence.remove(0);
 			} else {
+				
+				// 4. If the values are not equal, we simply continue on to the next element in the 'array' list.
 				continue;
 			}
 			
+			// 5. A final check in the loop sees if there are any elements left in the subsequence. If all have been removed since they exist in the 'array' list...
 			if(sequence.size() == 0) {
+				
+				// ...we return true, indicating the array contains the entire subsequence, breaking the loop.
 				return true;
 			}
 			
 		}
 		
+		// 6. If we make it through the entire array without the sequence's size equaling 0, then we know not all elements were found in the original. Thus we return 'false'.
 		return false;
 	}
 

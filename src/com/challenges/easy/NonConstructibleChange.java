15e2f27651fbae8e82d8775ab4f579744cda0917
Andrew Williams <1williamsaj@gmail.com>
2021-10-20 10:26:22 -0500

    Deleted old class for NonConstructableChange, created new class renamed as NonConstructibleChange to correct for misspelling

--- src/com/challenges/easy/NonConstructibleChange.java
+++ src/com/challenges/easy/NonConstructibleChange.java
@@ -1,6 +1,6 @@
 package com.challenges.easy;
 
-public class NonConstructableChange {
+public class NonConstructibleChange {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub

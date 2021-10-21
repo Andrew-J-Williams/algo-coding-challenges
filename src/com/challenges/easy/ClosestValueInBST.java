0a8a4089d37ae272f7c7ae584f30170a051714e8
Andrew Williams <1williamsaj@gmail.com>
2021-10-21 09:30:31 -0500

    Created ClosestValueInBST class, added appropriate functions

---
+++ src/com/challenges/easy/ClosestValueInBST.java
@@ -0,0 +1,25 @@
+package com.challenges.easy;
+
+public class ClosestValueInBST {
+
+	public static int findClosestValueInBst(BST tree, int target) {
+		// Write your code here.
+		return -1;
+	}
+
+	static class BST {
+		public int value;
+		public BST left;
+		public BST right;
+
+		public BST(int value) {
+			this.value = value;
+		}
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}

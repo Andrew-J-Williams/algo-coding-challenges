7c74c74162a92438e9bdf89d8a42d66228853e30
Andrew Williams <1williamsaj@gmail.com>
2021-10-21 14:52:04 -0500

    Added additional comments, created root and n1 for BST sample data

--- src/com/challenges/easy/ClosestValueInBST.java
+++ src/com/challenges/easy/ClosestValueInBST.java
@@ -11,6 +11,22 @@ package com.challenges.easy;
 	Each BST node has an integer value, a left child node, and a right child node. A node is said to be a valid BST node if and only if it satisfies the BST property: its
 	value is strictly greater than the values of every node to its left; its value is less than or equal to the values of every node to its right; and its children nodes are
 	either valid BST nodes
+	
+	Sample Input:
+	
+			tree = 10
+				 /    \
+			    5      15
+			   /  \   /  \
+			  2    5 13   22
+			 /        \
+			1         14
+			
+		target = 12
+		
+	
+	Sample Output:
+	13
  * 
  */
 
@@ -29,11 +45,17 @@ public class ClosestValueInBST {
 		public BST(int value) {
 			this.value = value;
 		}
+		
+		public void printBST() {
+			System.out.println(value);
+		}
 	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
+		BST root = new BST(10);
+		BST n1 = new BST(5);
 	}
 
 }

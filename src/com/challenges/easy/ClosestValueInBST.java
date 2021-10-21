bb0e4df45efcab9b374ff97f1c42513ca4f0971a
Andrew Williams <1williamsaj@gmail.com>
2021-10-21 15:23:40 -0500

    Added some comments for the logic of the program in ClosestValueInBST

--- src/com/challenges/easy/ClosestValueInBST.java
+++ src/com/challenges/easy/ClosestValueInBST.java
@@ -34,17 +34,28 @@ public class ClosestValueInBST {
 
 	public static int findClosestValueInBst(BST tree, int target) {
 		
+		// 1. We create a helper method that will run inside our main function, taking the root value of the tree and setting it as the closest value.
 		return findClosestValueInBstHelper(tree, target, tree.value);
 	}
 	
+	// 2. Our helper method is a recursive function, meaning it has the ability to call itself depending on the conditions.
 	public static int findClosestValueInBstHelper(BST tree, int target, int closest) {
 		
+		// 3. Within the function, we first see if the absolute difference between our target and current closest value is greater than the absolute difference between
+		// the target and the current node's value. If it is...
 		if (Math.abs(target - closest) > Math.abs(target - tree.value)){
+			
+			// ...then that node's value becomes the closest value. Otherwise we skip this condition altogether.
 			closest = tree.value; 
 		}
 		
+		// 4. If the target value is less than the node's value and the left node of that node is not null (meaning there is another node there)...
 		if(target < tree.value && tree.left != null){
+			
+			// ...then we return our helper function again, this time with the left node becoming the root value.
 			return findClosestValueInBstHelper(tree.left, target, closest);
+			
+		// 5.	
 		} else if (target > tree.value && tree.right != null) {
 			return findClosestValueInBstHelper(tree.right, target, closest);
 		} else {

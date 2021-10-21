ddec9fdade4f332e6b4ba7447e6d56be1503165d
Andrew Williams <1williamsaj@gmail.com>
2021-10-21 16:23:24 -0500

    Created new class called BranchSums, added code from the prompt

---
+++ src/com/challenges/easy/BranchSums.java
@@ -0,0 +1,30 @@
+package com.challenges.easy;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BranchSums {
+	
+	public static class BinaryTree {
+	    int value;
+	    BinaryTree left;
+	    BinaryTree right;
+
+	    BinaryTree(int value) {
+	      this.value = value;
+	      this.left = null;
+	      this.right = null;
+	    }
+	  }
+
+	  public static List<Integer> branchSums(BinaryTree root) {
+	    // Write your code here.
+	    return new ArrayList<Integer>();
+	  }
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}

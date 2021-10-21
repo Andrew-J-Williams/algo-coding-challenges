a3fb5ece35a8fa31c6aef82191b919f1816b6184
Andrew Williams <1williamsaj@gmail.com>
2021-10-21 15:09:35 -0500

    Found solution for ClosestValueInBST

--- src/com/challenges/easy/ClosestValueInBST.java
+++ src/com/challenges/easy/ClosestValueInBST.java
@@ -33,9 +33,27 @@ package com.challenges.easy;
 public class ClosestValueInBST {
 
 	public static int findClosestValueInBst(BST tree, int target) {
-		// Write your code here.
-		return -1;
+		
+		return findClosestValueInBstHelper(tree, target, tree.value);
 	}
+	
+	public static int findClosestValueInBstHelper(BST tree, int target, int closest) {
+		
+		if (Math.abs(target - closest) > Math.abs(target - tree.value)){
+			closest = tree.value; 
+		}
+		
+		if(target < tree.value && tree.left != null){
+			return findClosestValueInBstHelper(tree.left, target, closest);
+		} else if (target > tree.value && tree.right != null) {
+			return findClosestValueInBstHelper(tree.right, target, closest);
+		} else {
+			return closest;
+		}
+		
+	}
+	
+	
 
 	static class BST {
 		public int value;
@@ -56,6 +74,27 @@ public class ClosestValueInBST {
 		
 		BST root = new BST(10);
 		BST n1 = new BST(5);
+		BST n2 = new BST(15);
+		BST n3 = new BST(2);
+		BST n4 = new BST(5);
+		BST n5 = new BST(13);
+		BST n6 = new BST(22);
+		BST n7 = new BST(1);
+		BST n8 = new BST(14);
+		
+		root.left = n1;
+		root.right = n2;
+		root.left.left = n3;
+		root.left.right = n4;
+		root.right.left = n5;
+		root.right.right = n6;
+		root.left.left.left = n7;
+		root.right.left.right = n8;
+		
+		int targetVal = 12;
+		
+		System.out.println(findClosestValueInBst(root, targetVal));
+		
 	}
 
 }

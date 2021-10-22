d425f1a8e713dc7d74af20ec1ae3de8ccaae41b8
Andrew Williams <1williamsaj@gmail.com>
2021-10-22 18:26:19 -0500

    Reformatted the code in BranchSums. Added comment prompt.

--- src/com/challenges/easy/BranchSums.java
+++ src/com/challenges/easy/BranchSums.java
@@ -3,29 +3,44 @@ package com.challenges.easy;
 import java.util.ArrayList;
 import java.util.List;
 
-public class BranchSums {
+/*
+ * 
+	Branch Sums
 	
+	Write a function that takes in a Binary Tree and returns a list of its branch sums ordered from leftmost branch to
+	rightmost branch sum.
+	
+	A branch sum is the sum of all values in a Binary Tree branch. A Binary Tree branch is a path of nodes in a tree that
+	starts at the root node and ends at any leaf node.
+	
+	Each 'BinaryTree' node has an integer value, a left child node, and a right child node. Children nodes can either be
+	'BinaryTree' nodes themselves or 'None/null'.
+ * 
+ */
+
+public class BranchSums {
+
 	public static class BinaryTree {
-	    int value;
-	    BinaryTree left;
-	    BinaryTree right;
+		int value;
+		BinaryTree left;
+		BinaryTree right;
 
-	    BinaryTree(int value) {
-	      this.value = value;
-	      this.left = null;
-	      this.right = null;
-	    }
-	  }
+		BinaryTree(int value) {
+			this.value = value;
+			this.left = null;
+			this.right = null;
+		}
+	}
 
-	  public static List<Integer> branchSums(BinaryTree root) {
-	    // Write your code here.
-		  // Comment!
-	    return new ArrayList<Integer>();
-	  }
+	public static List<Integer> branchSums(BinaryTree root) {
+		// Write your code here.
+		// Comment!
+		return new ArrayList<Integer>();
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		BinaryTree root = new BinaryTree(1);
 	}
 
 }

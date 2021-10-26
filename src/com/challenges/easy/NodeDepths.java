b3a8ff3e1fb3dda7c9022559ecbfaf0f6fafcb53
Andrew Williams <1williamsaj@gmail.com>
2021-10-26 14:14:10 -0400

    Added new functions and test data to NodeDepths

--- src/com/challenges/easy/NodeDepths.java
+++ src/com/challenges/easy/NodeDepths.java
@@ -2,9 +2,45 @@ package com.challenges.easy;
 
 public class NodeDepths {
 
+	public static class BinaryTree {
+		int value;
+		BinaryTree left;
+		BinaryTree right;
+
+		BinaryTree(int value) {
+			this.value = value;
+			this.left = null;
+			this.right = null;
+		}
+	}
+
+	public static int nodeDepths(BinaryTree root) {
+		// Write your code here.
+		return -1;
+	}
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		BinaryTree root = new BinaryTree(1);
+		BinaryTree n1 = new BinaryTree(2);
+		BinaryTree n2 = new BinaryTree(3);
+		BinaryTree n3 = new BinaryTree(4);
+		BinaryTree n4 = new BinaryTree(5);
+		BinaryTree n5 = new BinaryTree(6);
+		BinaryTree n6 = new BinaryTree(7);
+		BinaryTree n7 = new BinaryTree(8);
+		BinaryTree n8 = new BinaryTree(9);
+		BinaryTree n9 = new BinaryTree(10);
 
+		root.left = n1;
+		root.right = n2;
+		root.left.left = n3;
+		root.left.right = n4;
+		root.right.left = n5;
+		root.right.right = n6;
+		root.left.left.left = n7;
+		root.left.left.right = n8;
+		root.left.right.left = n9;
 	}
 
 }

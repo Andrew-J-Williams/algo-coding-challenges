f4b00faecec149deeaeb0fb8dde33c228ab25f80
Andrew Williams <1williamsaj@gmail.com>
2021-10-26 16:37:04 -0400

    Created a new class under the easy challenges called MinimumWaitingTime

--- src/com/challenges/easy/DepthFirstSearch.java
+++ src/com/challenges/easy/DepthFirstSearch.java
@@ -61,7 +61,9 @@ public class DepthFirstSearch {
 	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		List<String> array = new ArrayList<>();
+		
+		
 
 	}
 
---
+++ src/com/challenges/easy/MinimumWaitingTime.java
@@ -0,0 +1,10 @@
+package com.challenges.easy;
+
+public class MinimumWaitingTime {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}

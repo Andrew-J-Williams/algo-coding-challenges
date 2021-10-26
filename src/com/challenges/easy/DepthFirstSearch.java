f9d9e647377cc749e3abf3cc0d1df2343cebf03e
Andrew Williams <1williamsaj@gmail.com>
2021-10-26 15:09:11 -0400

    Added code from prompt to the DepthFirstSearch class

--- src/com/challenges/easy/DepthFirstSearch.java
+++ src/com/challenges/easy/DepthFirstSearch.java
@@ -1,6 +1,33 @@
 package com.challenges.easy;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class DepthFirstSearch {
+
+	// Do not edit the class below except
+	// for the depthFirstSearch method.
+	// Feel free to add new properties
+	// and methods to the class.
+	static class Node {
+		String name;
+		List<Node> children = new ArrayList<Node>();
+
+		public Node(String name) {
+			this.name = name;
+		}
+
+		public List<String> depthFirstSearch(List<String> array) {
+			// Write your code here.
+			return array;
+		}
+
+		public Node addChild(String name) {
+			Node child = new Node(name);
+			children.add(child);
+			return this;
+		}
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub

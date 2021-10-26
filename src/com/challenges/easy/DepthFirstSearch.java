970b2de6acec0d1965e07fc62fc7f4d112e11b8f
Andrew Williams <1williamsaj@gmail.com>
2021-10-26 16:09:20 -0400

    Added comment prompt to DepthFirstSearch

--- src/com/challenges/easy/DepthFirstSearch.java
+++ src/com/challenges/easy/DepthFirstSearch.java
@@ -3,6 +3,32 @@ package com.challenges.easy;
 import java.util.ArrayList;
 import java.util.List;
 
+/*
+ * 
+	Depth-first Search
+	
+	You're given a Node class that has a name and an array of optional children nodes. When put together, nodes form an acyclic tree-like structure.
+	
+	Implement the depthFirstSearch method on the Node class, which takes in an empty array, traverse the tree using a Depth-first Search approach (specifically navigating the tree from
+	left to right), stores all the nodes' names in the input array, and returns it.
+	
+	Sample Input:
+	
+			graph = A
+			 	 /  |  \
+			 	B   C   D
+			   / \     / \
+			  E   F   G   H
+			  	 / \   \
+                I   J   K
+                
+ 	Sample Output:
+ 	[ "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K" ]
+ * 
+ * 
+ */
+
+
 public class DepthFirstSearch {
 
 	// Do not edit the class below except

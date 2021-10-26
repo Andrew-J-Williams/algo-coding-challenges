b70933030a4639e218bb14cf9f68bc4a3f133acd
Andrew Williams <1williamsaj@gmail.com>
2021-10-26 15:04:02 -0400

    Added comments to explain the logic of NodeDepths

--- src/com/challenges/easy/NodeDepths.java
+++ src/com/challenges/easy/NodeDepths.java
@@ -42,31 +42,47 @@ public class NodeDepths {
 		}
 	}
 	
+	// 1. We create a recursive function that will keep track of how many levels down each branch we are and add each node on those levels to an array of node depths.
 	public static void calculateNodeDepths(BinaryTree node, int depth, List<Integer> runningSum) {
+		
+		// 2. We check and see if the node is empty or equal to null. If it is...
 		if(node == null) {
+			
+			// ...we simply return the function, which will mark the end of a branch and update our array of node depths.
 			return;
 		}
-				
+		
+		// 3. If it is not null, we add the current depth level to our ArrayList of node depths.
 		runningSum.add(depth);
 		
+		// 4. We increase the value of the depth by 1 since we will be descending deeper along the branches.
 		int newDepth = depth + 1;
 		
-		System.out.println(runningSum);
-		
+		// 5. We account for this node having children on the left and right by calling our recursive function on both possibilities, repeating the process until we reach all leaf nodes.
 		calculateNodeDepths(node.left, newDepth, runningSum);
 		calculateNodeDepths(node.right, newDepth, runningSum);
 	}
 
+	// 6. In our main function, we take in the root of the BinaryTree as our only argument.
 	public static int nodeDepths(BinaryTree root) {
+		
+		// 7. We define and ArrayList that will hold the depth levels of every node in the BinaryTree. 
 		List<Integer> sums = new ArrayList<>();
+		
+		// 8. We create a variable that will hold the sum of all the node depths in the BinaryTree.
 		int sum = 0;
 		
+		// 9. We call our recursive function, passing in our root, 0 (representing the level of the root), and our sums ArrayList.
 		calculateNodeDepths(root, 0, sums);
 		
+		// 10. After the recursive function finishes its run, we create a 'for' loop to iterate over every value in the ArrayList. For each value...
 		for(int x : sums) {
+			
+			// ... we add it to our running total until we reach the end of the list.
 			sum += x;
 		}
 		
+		// 11. Finally, we return the sum of all the node depths.
 		return sum;
 	}
 

c4cedd323e1e5f88eeaf531fe4d16df91c948fc6
Andrew Williams <1williamsaj@gmail.com>
2021-10-27 10:14:59 -0400

    Added comment prompt to MinimumWaitingTime

--- src/com/challenges/easy/MinimumWaitingTime.java
+++ src/com/challenges/easy/MinimumWaitingTime.java
@@ -1,5 +1,26 @@
 package com.challenges.easy;
 
+/*
+ * 
+	Minimum Waiting Time
+	
+	You're given a non-empty array of positive integers representing the amounts of time that specific queries take to execute. Only one query can be executed at a time, but the queries
+	can be executed in any order.
+	
+	A query's 'waiting time' is defined as the amount of time it must wait before its execution can start. In other words, if a query is executed second, then its waiting time is the
+	duration of the first query; if a query is executed third, then its waiting time is the sum of the durations of the first two queries.
+	
+	Write a function that returns the minimum amount of total waiting time for all the queries.
+	
+	Sample Input:
+	[ 3, 2, 1, 2, 6 ]
+	
+	Sample Output:
+	16
+	
+ * 
+ */
+
 public class MinimumWaitingTime {
 
 	public static void main(String[] args) {

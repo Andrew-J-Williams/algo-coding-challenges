a806d9ba83f0ebfaeffa5300c21a7d2791efc393
Andrew Williams <1williamsaj@gmail.com>
2021-10-28 11:12:06 -0400

    Finished writing the comment prompt for TandemBicycle

--- src/com/challenges/easy/TandemBicycle.java
+++ src/com/challenges/easy/TandemBicycle.java
@@ -11,7 +11,18 @@ package com.challenges.easy;
 	is represented by a single, positive integer, which is the speed they pedal the tandem bike at. Both lists are the same length, meaning there are just as many red shirt riders as there
 	are blue shirt riders. Your goal is to pair every rider wearing a red shirt with a rider wearing a blue shirt to operate the tandem bicycle.
 	
+	Write a function that determines the maximum total speed or the minimum possible total speed of all the tandem bicycles being ridden based on an input parameter, 'fastest'. If 
+	'fastest = true', then your function should return the maximum possible total speed; otherwise it should return the minimum total speed.
 	
+	"Total speed" is defined as the sum of all the tandem bicycles being ridden. 
+	
+	Sample Input:
+	redShirtSpeeds = [ 5, 5, 3, 9, 2 ]
+	blueShirtSpeeds = [ 3, 6, 7, 2, 1 ]
+	fastest = true
+	
+	Sample Output:
+	32
  * 
  */
 

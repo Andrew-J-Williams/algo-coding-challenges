f4347e163903ef8a0a8a5b918e15f6e180d41e1e
Andrew Williams <1williamsaj@gmail.com>
2021-10-28 09:27:09 -0400

    Added initial code and started comment section at the top for prompt

--- src/com/challenges/easy/ClassPhotos.java
+++ src/com/challenges/easy/ClassPhotos.java
@@ -1,6 +1,19 @@
 package com.challenges.easy;
 
+import java.util.ArrayList;
+
+/*
+ * 
+ * 
+ * 
+ */
+
 public class ClassPhotos {
+
+	public boolean classPhotos(ArrayList<Integer> redShirtHeights, ArrayList<Integer> blueShirtHeights) {
+		// Write your code here.
+		return false;
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub

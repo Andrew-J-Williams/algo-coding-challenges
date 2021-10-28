eec4ea8d72232d9c2f2abb1e2f9c0f36db29c9de
Andrew Williams <1williamsaj@gmail.com>
2021-10-28 09:47:35 -0400

    Added comment prompt for ClassPhotos

--- src/com/challenges/easy/ClassPhotos.java
+++ src/com/challenges/easy/ClassPhotos.java
@@ -4,7 +4,22 @@ import java.util.ArrayList;
 
 /*
  * 
- * 
+	Class Photos
+	
+	It's photo day at the local school, and you are the photographer assigned to take class photos. The class you'll be photographing has an even number of students, and all these students
+	are wearing either red or blue shirts. In fact, exactly half the class is wearing red shirts, and the other half is wearing blue shirts. You're responsible for arranging the students
+	in two rows before taking the photo. Each row should contain the same number of students and adhere to the following guidelines:
+	
+		- All students wearing red shirts should be in the same row.
+		- All students wearing blue shirts should be in the same row.
+		- Each student in the back row must be strictly taller than the student directly in front of them in the front row.
+		
+	You're given two input arrays: one containing the heights of all students with red shirts and another containing the heights of all students with blue shirts. These arrays will always
+	be the same length, and each height will be a positive integer. Write a function that determines whether or not a class photo that follows the stated guidelines can be taken.
+	
+	Note: You can assume that each class contains at least 2 students.
+	
+	
  * 
  */
 

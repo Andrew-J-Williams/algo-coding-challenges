f791068650a347f3cc9046b0cbade340179031b3
Andrew Williams <1williamsaj@gmail.com>
2021-10-28 11:06:36 -0400

    Writing first half of comment prompt for TandemBicycle

--- src/com/challenges/easy/TandemBicycle.java
+++ src/com/challenges/easy/TandemBicycle.java
@@ -1,7 +1,28 @@
 package com.challenges.easy;
 
+/*
+ * 
+	Tandem Bicycle
+	
+	A tandem bicycle is a bicycle operated by 2 people: person A and person B. Both people pedal the bicycle, but the person who pedals faster determines the speed of the bicycle. So if
+	person A pedals at a speed of '5' and person B pedals at a speed of '4', the tandem bicycle moves at a speed of 5.
+	
+	You're given two lists of positive integers: one that contains the speeds of the riders wearing red shirts and another that contains the speeds of riders wearing blue shirts. Each rider
+	is represented by a single, positive integer, which is the speed they pedal the tandem bike at. Both lists are the same length, meaning there are just as many red shirt riders as there
+	are blue shirt riders. Your goal is to pair every rider wearing a red shirt with a rider wearing a blue shirt to operate the tandem bicycle.
+	
+	
+ * 
+ */
+
+
 public class TandemBicycle {
 
+	public int tandemBicycle(int[] redShirtSpeeds, int[] blueShirtSpeeds, boolean fastest) {
+	    // Write your code here.
+	    return -1;
+	}
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 

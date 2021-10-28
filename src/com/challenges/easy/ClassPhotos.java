407af3c840bdb8ce7827e7841d017d704bc84e65
Andrew Williams <1williamsaj@gmail.com>
2021-10-28 09:30:58 -0400

    Added test data in main file of ClassPhotos

--- src/com/challenges/easy/ClassPhotos.java
+++ src/com/challenges/easy/ClassPhotos.java
@@ -10,13 +10,28 @@ import java.util.ArrayList;
 
 public class ClassPhotos {
 
-	public boolean classPhotos(ArrayList<Integer> redShirtHeights, ArrayList<Integer> blueShirtHeights) {
+	public static boolean classPhotos(ArrayList<Integer> redShirtHeights, ArrayList<Integer> blueShirtHeights) {
 		// Write your code here.
 		return false;
 	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		ArrayList<Integer> redShirts1 = new ArrayList<>();
+		ArrayList<Integer> blueShirts1 = new ArrayList<>();
+		
+		redShirts1.add(5);
+		redShirts1.add(8);
+		redShirts1.add(1);
+		redShirts1.add(3);
+		redShirts1.add(4);
+		
+		blueShirts1.add(6);
+		blueShirts1.add(9);
+		blueShirts1.add(2);
+		blueShirts1.add(4);
+		blueShirts1.add(5);
+		
+		System.out.println(classPhotos(redShirts1, blueShirts1));
 
 	}
 

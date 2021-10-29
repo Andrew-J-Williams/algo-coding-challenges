730fd4c207d1e4b8b30312d485d131458c51a6a0
Andrew Williams <1williamsaj@gmail.com>
2021-10-29 10:23:08 -0400

    Added code from prompt to LinkedList problem

--- src/com/challenges/easy/RemoveDuplicatesFromLinkedList.java
+++ src/com/challenges/easy/RemoveDuplicatesFromLinkedList.java
@@ -1,6 +1,23 @@
 package com.challenges.easy;
 
 public class RemoveDuplicatesFromLinkedList {
+	
+	public static class LinkedList {
+	    public int value;
+	    public LinkedList next;
+
+	    public LinkedList(int value) {
+	      this.value = value;
+	      this.next = null;
+	    }
+	  }
+
+	  public LinkedList removeDuplicatesFromLinkedList(LinkedList linkedList) {
+	    
+			System.out.println(linkedList);
+			
+	    return linkedList;
+	  }
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub

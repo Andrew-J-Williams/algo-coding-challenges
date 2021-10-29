129c49b1744d1856e644036dd02e01ee31dc175f
Andrew Williams <1williamsaj@gmail.com>
2021-10-29 13:47:09 -0400

    Added prompt code to NthFibonacci

--- src/com/challenges/easy/NthFibonacci.java
+++ src/com/challenges/easy/NthFibonacci.java
@@ -2,6 +2,11 @@ package com.challenges.easy;
 
 public class NthFibonacci {
 
+	public static int getNthFib(int n) {
+		// Write your code here.
+		return -1;
+	}
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 

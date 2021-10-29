914640d5797277b81bee8b5b7a23269fbf8390f2
Andrew Williams <1williamsaj@gmail.com>
2021-10-29 15:39:58 -0400

    Added a comment prompt to RemoveDuplicate

--- src/com/challenges/easy/RemoveDuplicatesFromLinkedList.java
+++ src/com/challenges/easy/RemoveDuplicatesFromLinkedList.java
@@ -1,5 +1,26 @@
 package com.challenges.easy;
 
+
+/*
+ * 
+	Removes Duplicates from Linked List
+	
+	You've been given the head of a Singly Linked List whose nodes are in order with respect to their values. Write a function that returns a modified version of the Linked List that
+	doesn't contain any nodes with duplicate values. The Linked List should be modified in place (i.e. you shouldn't create a brand new list), and the modified Linked List should still
+	have its nodes sorted with respect to value.
+	
+	Each LinkedList node has an integer value as well as a 'next' node pointing to the next node on the list or to 'None/null' if it's the tail of the list.
+	
+	Sample Input:
+	linkedList = 1 -> 1 -> 3 -> 4 -> 4 -> -> 4 -> 5 -> 6 -> 6
+	
+	Sample Output:
+	1 -> 3 -> 4 -> 5 -> 6
+	
+ * 
+ */
+
+
 public class RemoveDuplicatesFromLinkedList {
 
 	public static class LinkedList {

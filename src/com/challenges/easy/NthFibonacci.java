7adfb8b64d9df164bd6eb684409f7aa34b84fa5c
Andrew Williams <1williamsaj@gmail.com>
2021-10-29 15:48:59 -0400

    Added test data to NthFibonacci

--- src/com/challenges/easy/NthFibonacci.java
+++ src/com/challenges/easy/NthFibonacci.java
@@ -1,5 +1,24 @@
 package com.challenges.easy;
 
+/*
+ * 
+	Nth Fibonacci
+	
+	The Fibonacci sequence is defined as follows: the first number of the sequence (0), the second number of the sequence (1), and the 'nth' number is the sum of (n-1)th and
+	(n-2)th numbers. Write a function takes in an integer 'n' and returns the 'nth' fibonacci number.
+	
+	Important note: the Fibonacci sequence is often defined with its first two numbers as F0 = 0 and F1 = 1. For the purpose of this question, the first Fibonacci number is F0; therefore,
+	getNthFib(1) is equal to F0 and getNthFib(2) is equal to F1, and so on.
+	
+	Sample Input:
+	n = 6
+	
+	Sample Output:
+	5 // 0, 1, 1, 2, 3, 5
+	
+ * 
+ */
+
 public class NthFibonacci {
 
 	public static int getNthFib(int n) {
@@ -8,7 +27,11 @@ public class NthFibonacci {
 	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int n = 5;
+		int n2 = 2;
+		
+		System.out.println(getNthFib(n));
+		System.out.println(getNthFib(n2));
 
 	}
 

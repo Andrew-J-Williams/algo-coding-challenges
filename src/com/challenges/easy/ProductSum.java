d50352ecbd456bf7a128219b62c16ffa9da6b751
Andrew Williams <1williamsaj@gmail.com>
2021-10-30 22:56:30 -0400

    Started writing the comment prompt for Product Sum

--- src/com/challenges/easy/ProductSum.java
+++ src/com/challenges/easy/ProductSum.java
@@ -1,5 +1,15 @@
 package com.challenges.easy;
 
+/*
+ * 
+	Product Sum
+	
+	Write a function that takes in a special array and returns its product sum.
+	
+	A "special array" is a non-empty array that either contains integers or other "special" arrays. The product sum of a "special array" is the sum of its elements,  
+ * 
+ */
+
 public class ProductSum {
 
 	public static void main(String[] args) {

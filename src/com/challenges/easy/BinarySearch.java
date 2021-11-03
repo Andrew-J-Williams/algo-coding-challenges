7aecee2f0ab8629951b6ddc881a9f253505f5125
Andrew Williams <1williamsaj@gmail.com>
2021-11-03 13:47:46 -0400

    Added comment logic to BinarySearch

--- src/com/challenges/easy/BinarySearch.java
+++ src/com/challenges/easy/BinarySearch.java
@@ -23,25 +23,45 @@ package com.challenges.easy;
 
 public class BinarySearch {
 
+	// 1. We have our main function that accepts the array and the target value, returning the integer result of our recursive function.
 	public static int binarySearch(int[] array, int target) {
 		
 		return binaryCheck(array, target, 0, array.length - 1);
+		
 	}
 	
+	// 2. Our recursive function accepts the array, the target, a left pointer and a right pointer, indicating opposite ends of the array.
 	public static int binaryCheck(int[] array, int target, int left, int right) {
 		
+		// 3. If our left pointer is greater than the right...
 		if(left > right) {
+			
+			// ...we know that our target value was not found, so we return -1. 
 			return -1;
+			
 		}
 		
+		// 4. Otherwise, we create a variable 'm' and set it equal to the middle value of our left and right values.
 		int m = (left + right) / 2;
+		
+		// 5. We create a variable to equal the index value of 'm'.
 		int match = array[m];
 		
+		// 6. If our match is equal to the target value...
 		if(target == match) {
+			
+			// ...then we simply return 'm', representing the index of the target value.
 			return m;
+		// 7. Otherwise, if the target is less than the match, we know that all values to the right of 'm' are greater than the target, eliminating half the array...	
 		} else if(target < match) {
+			
+			// ...so we return our recursive function with our right pointer's value minus 1, shrinking the search area.
 			return binaryCheck(array, target, left, m - 1);
+			
+		// 8. Finally, if the target value is greater than match, it means the opposite: all values to the left of 'm' are less than the target value...	
 		} else {
+			
+			// ... so we simply return our recursive function with our left pointer plus 1.
 			return binaryCheck(array, target, m + 1, right);
 		}
 		

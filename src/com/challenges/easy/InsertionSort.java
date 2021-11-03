3a58755e5f3b56992a02ce51dbdf90cac35e7837
Andrew Williams <1williamsaj@gmail.com>
2021-11-03 15:31:35 -0400

    Added prompt code to InsertionSort

--- src/com/challenges/easy/InsertionSort.java
+++ src/com/challenges/easy/InsertionSort.java
@@ -2,6 +2,11 @@ package com.challenges.easy;
 
 public class InsertionSort {
 
+	public static int[] insertionSort(int[] array) {
+		// Write your code here.
+		return new int[] {};
+	}
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 

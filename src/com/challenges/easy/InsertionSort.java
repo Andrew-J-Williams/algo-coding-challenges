dedf8483bbb1e4ad9cd0cb776d716079ddef804b
Andrew Williams <1williamsaj@gmail.com>
2021-11-03 15:33:40 -0400

    Added comment prompt to InsertionSort

--- src/com/challenges/easy/InsertionSort.java
+++ src/com/challenges/easy/InsertionSort.java
@@ -1,5 +1,19 @@
 package com.challenges.easy;
 
+/*
+ * 
+	Insertion Sort
+	
+	Write a function that takes in an array of integers and returns a sorted version of that array. Use the Insertion Sort algorithm to sort the array.
+	
+	Sample Input:
+	array = [ 8, 5, 2, 9, 5, 6, 3 ]
+	
+	Sample Output:
+	[ 2, 3, 5, 5, 6 , 8, 9 ] 
+ * 
+ */
+
 public class InsertionSort {
 
 	public static int[] insertionSort(int[] array) {

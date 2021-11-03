230d4d680e2bf47e4ad994feb7a7944088dbffcd
Andrew Williams <1williamsaj@gmail.com>
2021-11-03 12:47:27 -0400

    Added prompt code to BinarySearch

--- src/com/challenges/easy/BinarySearch.java
+++ src/com/challenges/easy/BinarySearch.java
@@ -2,6 +2,11 @@ package com.challenges.easy;
 
 public class BinarySearch {
 
+	public static int binarySearch(int[] array, int target) {
+		// Write your code here.
+		return -1;
+	}
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 

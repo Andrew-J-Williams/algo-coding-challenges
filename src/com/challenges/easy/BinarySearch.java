5dc49dd6bb03fccae70c66253e03794dbae0d180
Andrew Williams <1williamsaj@gmail.com>
2021-11-03 13:03:58 -0400

    Added initial code for solution of Binary Search

--- src/com/challenges/easy/BinarySearch.java
+++ src/com/challenges/easy/BinarySearch.java
@@ -7,6 +7,8 @@ package com.challenges.easy;
 	Write a function that takes in a sorted array of integers as well as a target integer. The function should use the Binary Search algorithm to determine if the
 	target integer is contained in the array and should return its index if it is, otherwise -1.
 	
+	Binary search 
+	
 	Sample Input:
 	array = [ 0, 1, 21, 33, 45, 45, 61, 71, 72, 73 ]
 	target = 33
@@ -21,6 +23,10 @@ public class BinarySearch {
 
 	public static int binarySearch(int[] array, int target) {
 		// Write your code here.
+		
+		int left = 0;
+		int right = array.length - 1;
+		
 		return -1;
 	}
 

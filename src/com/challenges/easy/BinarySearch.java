8698eb49a06f06929863631aa8c3169131b7ca81
Andrew Williams <1williamsaj@gmail.com>
2021-11-03 13:06:50 -0400

    Added clarification in comment prompt of BinarySearch

--- src/com/challenges/easy/BinarySearch.java
+++ src/com/challenges/easy/BinarySearch.java
@@ -7,7 +7,9 @@ package com.challenges.easy;
 	Write a function that takes in a sorted array of integers as well as a target integer. The function should use the Binary Search algorithm to determine if the
 	target integer is contained in the array and should return its index if it is, otherwise -1.
 	
-	Binary search 
+	Binary search involves breaking down our set of values continuously in half until we find the value we are searching for. Take the example of a college professor
+	searching through a stack of tests, all arranged in alphabetical order by first name. If the name we are looking for is "Jenny", we know if we open the stack to
+	the middle all tests to the right can be eliminated. We continue the process until finding the paper.
 	
 	Sample Input:
 	array = [ 0, 1, 21, 33, 45, 45, 61, 71, 72, 73 ]

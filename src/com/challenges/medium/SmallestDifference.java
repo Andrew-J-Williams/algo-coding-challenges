15ca8c613103dce81fbc7ef9e589ebb5d10b5834
Andrew Williams <1williamsaj@gmail.com>
2021-11-04 14:33:06 -0500

    Added prompt code to SmallestDifference

--- src/com/challenges/medium/SmallestDifference.java
+++ src/com/challenges/medium/SmallestDifference.java
@@ -2,6 +2,11 @@ package com.challenges.medium;
 
 public class SmallestDifference {
 
+	public static int[] smallestDifference(int[] arrayOne, int[] arrayTwo) {
+		// Write your code here.
+		return new int[] {};
+	}
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 

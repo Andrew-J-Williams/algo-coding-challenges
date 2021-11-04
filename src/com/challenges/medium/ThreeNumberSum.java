6ccab89482afb3d18b06b0de50d3f5d490c08788
Andrew Williams <1williamsaj@gmail.com>
2021-11-04 12:03:00 -0500

    Added prompt code to ThreeNumberSum

--- src/com/challenges/medium/ThreeNumberSum.java
+++ src/com/challenges/medium/ThreeNumberSum.java
@@ -1,6 +1,14 @@
 package com.challenges.medium;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ThreeNumberSum {
+
+	public static List<Integer[]> threeNumberSum(int[] array, int targetSum) {
+		// Write your code here.
+		return new ArrayList<Integer[]>();
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub

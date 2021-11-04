36491e3d30d208c1580cd9b995b9bf8f713e4dae
Andrew Williams <1williamsaj@gmail.com>
2021-11-04 10:41:11 -0500

    Added comment logic for GenerateDocument

--- src/com/challenges/easy/GenerateDocument.java
+++ src/com/challenges/easy/GenerateDocument.java
@@ -30,31 +30,50 @@ public class GenerateDocument {
 
 	public static boolean generateDocument(String characters, String document) {
 
+		// 1. We create a 'for' loop that will iterate through the length of our document String.
 		for(int i = 0; i < document.length(); i++) {
+			
+			// 2. We create a variable that will hold the individual character of each index in our document String as we iterate through it, allowing us to check each character.
 			char character = document.charAt(i);
 			
+			// 3. We create two variables equal to the result of our 'count' helper method, which will be the frequency of the character in the value of an integer.
 			int documentFrequency = count(character, document);
 			int charactersFrequency = count(character, characters);
 			
+			// 4. We check to see if the character's frequency within the document is greater than its frequency in our string of characters. If it is...
 			if(documentFrequency > charactersFrequency) {
+				
+				// ...we return false, since our characters String MUST have the exact amount of characters and spaces that are present in the document String in order to match.
 				return false;
 			}
 		}
 		
+		// 5. If the loop completes and the frequency of characters is less than or equal between the document and the character String, we know all characters are accounted for
+		// and we can return true.
 		return true;
 	}
 	
+	// 6. Our helper method 'count' accepts two parameters: the specific character we are checking, as well as the target String we are searching for it in.
 	public static int count(char character, String target) {
+		
+		// 7. We create a variable that will hold the number of times the character in question appears in the target String.
 		int frequency = 0;
 		
+		// 8. We create a 'for' loop that will iterate over the target String.
 		for(int i = 0; i < target.length(); i++) {
+			
+			// 9. We create a variable that holds the character at index 'i' in the String. This way we can check each individual character.
 			char c = target.charAt(i);
 			
+			// 10. If the character we are checking is equal to the character at the given index 'i'...
 			if(c == character) {
+				
+				// ...we increment our frequency variable by one.
 				frequency += 1;
 			}
 		}
 		
+		// 11. Once all characters in the String have been checked, we return our final frequency value, representing all the instances of that given character in the target String.
 		return frequency;
 	}
 

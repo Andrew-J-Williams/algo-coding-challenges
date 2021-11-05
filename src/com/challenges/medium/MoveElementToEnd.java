9ea7b20bb3a55fa8ce95d62f17b3a3ed01ac6f26
Andrew Williams <1williamsaj@gmail.com>
2021-11-05 09:58:12 -0500

    Added comment logic to MoveElementToEnd

--- src/com/challenges/medium/MoveElementToEnd.java
+++ src/com/challenges/medium/MoveElementToEnd.java
@@ -15,34 +15,60 @@ import java.util.*;
 public class MoveElementToEnd {
 
 	public static List<Integer> moveElementToEnd(List<Integer> array, int toMove) {
-		
-		if(array.size() == 0) {
-			return new ArrayList<Integer>();
-		}
-		
+
+		// 1. We create 2 pointers and set them equal to the first and last indexes of
+		// the ArrayList.
 		int p1 = 0;
 		int p2 = array.size() - 1;
-		
-		while(p1 < p2) {
-			if(array.get(p1) == toMove && array.get(p2) == toMove) {
+
+		// 2. We create a 'while' loop that runs until p1 is greater than p2, touching
+		// every value in the ArrayList.
+		while (p1 < p2) {
+
+			// 3. We create another 'while' loop that runs if p1 is less than p2 an the
+			// value at p2 equals 'toMove'. While these conditions remain true...
+			while (p1 < p2 && array.get(p2) == toMove) {
+
+				// ...we decrement p2 by 1 since our target value is currently at the end of the
+				// ArrayList, like we want it.
 				p2--;
-			} else if(array.get(p1) == toMove && array.get(p2) != toMove) {
-				int temp = array.get(p2);
-				array.set(p2, array.get(p1));
-				array.set(p1, temp);
-				p1++;
-			} else {
-				p1++;
 			}
+
+			// 4. After the loop is broken, we check if the value at p1 equal the value of
+			// 'toMove'. If it does...
+			if (array.get(p1) == toMove) {
+				
+				// ...we use our helper method 'swap' to switch the values at p1 and p2, effectively moving the target value to the end of the array...
+				swap(p1, p2, array);
+				
+				// ...and then we increment p1 by 1 to check the next value to the right.
+				
+			}
+			
+			// 5. Once the swap has been made, we increment p1 by 1 and repeat the while loop until conditions become false.
+			p1++;
 		}
-		
-		
+
+		// 6. Finally, after we've checked every value and the loop breaks, we return our modified (in-place) array.
 		return array;
+	}
+
+	// 7. Our helper method 'swap' accepts the pointers and the array as parameters.
+	public static void swap(int p1, int p2, List<Integer> array) {
+		
+		// 8. We create a 'temp' variable to store the value found at p2...
+		int temp = array.get(p2);
+		
+		// ...we then set the value of p2 to be the value found at p1...
+		array.set(p2, array.get(p1));
+		
+		// ...and finally set the value of p1 to be our 'temp' variable, swapping both values without creating a new ArrayList.
+		array.set(p1, temp);
 	}
 
 	public static void main(String[] args) {
 		List<Integer> array1 = new ArrayList<>();
-		
+
 		array1.add(2);
 		array1.add(1);
 		array1.add(2);
@@ -51,11 +77,10 @@ public class MoveElementToEnd {
 		array1.add(3);
 		array1.add(4);
 		array1.add(2);
-		
+
 		int move1 = 2;
-		
+
 		System.out.println(moveElementToEnd(array1, move1));
-		
 
 	}
 

081f45bcba05c5ccdf021bb5749e1886aea62e43
Andrew Williams <1williamsaj@gmail.com>
2021-11-05 09:01:10 -0500

    Created a new class called MoveElementToEnd under medium challenges

---
+++ src/com/challenges/medium/MoveElementToEnd.java
@@ -0,0 +1,17 @@
+package com.challenges.medium;
+
+import java.util.*;
+
+public class MoveElementToEnd {
+
+	public static List<Integer> moveElementToEnd(List<Integer> array, int toMove) {
+		// Write your code here.
+		return new ArrayList<Integer>();
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}

55c5d9292f0a1f23fdb671483c4bd2e0454ad68b
Andrew Williams <1williamsaj@gmail.com>
2021-11-05 10:02:38 -0500

    Added prompt code to MonotonicArray

--- src/com/challenges/medium/MonotonicArray.java
+++ src/com/challenges/medium/MonotonicArray.java
@@ -2,6 +2,11 @@ package com.challenges.medium;
 
 public class MonotonicArray {
 
+	public static boolean isMonotonic(int[] array) {
+		// Write your code here.
+		return false;
+	}
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 

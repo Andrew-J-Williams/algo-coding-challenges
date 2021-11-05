76cb9ab0805df88964b098ca278b5b32834e81f4
Andrew Williams <1williamsaj@gmail.com>
2021-11-05 09:31:56 -0500

    Found coding solution for MoveElementToEnd

--- src/com/challenges/medium/MoveElementToEnd.java
+++ src/com/challenges/medium/MoveElementToEnd.java
@@ -15,12 +15,47 @@ import java.util.*;
 public class MoveElementToEnd {
 
 	public static List<Integer> moveElementToEnd(List<Integer> array, int toMove) {
-		// Write your code here.
-		return new ArrayList<Integer>();
+		
+		if(array.size() == 0) {
+			return new ArrayList<Integer>();
+		}
+		
+		int p1 = 0;
+		int p2 = array.size() - 1;
+		
+		while(p1 < p2) {
+			if(array.get(p1) == toMove && array.get(p2) == toMove) {
+				p2--;
+			} else if(array.get(p1) == toMove && array.get(p2) != toMove) {
+				int temp = array.get(p2);
+				array.set(p2, array.get(p1));
+				array.set(p1, temp);
+				p1++;
+			} else {
+				p1++;
+			}
+		}
+		
+		
+		return array;
 	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		List<Integer> array1 = new ArrayList<>();
+		
+		array1.add(2);
+		array1.add(1);
+		array1.add(2);
+		array1.add(2);
+		array1.add(2);
+		array1.add(3);
+		array1.add(4);
+		array1.add(2);
+		
+		int move1 = 2;
+		
+		System.out.println(moveElementToEnd(array1, move1));
+		
 
 	}
 

0e66c639cef15db2b601bf560768d36d2a88554d
Andrew Williams <1williamsaj@gmail.com>
2021-11-05 15:02:15 -0500

    Added comment logic for MonotonicArray

--- src/com/challenges/medium/MonotonicArray.java
+++ src/com/challenges/medium/MonotonicArray.java
@@ -19,21 +19,35 @@ public class MonotonicArray {
 
 	public static boolean isMonotonic(int[] array) {
 
+		// 1. We create 2 boolean variables, one representing if the values in the array are non-increasing and one representing if they are non-decreasing. We set both equal to
+		// true since we have not determined what our array will be yet.
 		boolean isNonIncreasing = true;
 		boolean isNonDecreasing = true;
 		
+		// 2. We create a for loop that starts at the 2nd element in the array and iterates over each until the end of the array. We start at the 2nd element because we are going
+		// to continuously be comparing the current element to the element left of it in an attempt to see if the values are increasing or decreasing. 
 		for(int i = 1; i < array.length; i++) {
 			
+			// 3. We check if our current value is less than the value behind it. If it is, we know that the array should be classified as non-increasing. As a result...
 			if(array[i] < array[i - 1]) {
+				
+				// ...we set our non-decreasing variable equal to false, leaving our non-increasing as true.
 				isNonDecreasing = false;
 			}
 			
+			// 4. If the current value is greater than the previous, we know the array should be non-decreasing in nature. As a result...
 			if(array[i] > array[i - 1]) {
+				
+				// ...we set non-increasing to false, since each new value should be greater than or equal to the previous value.
 				isNonIncreasing = false;
 			}
 			
+			// NOTE: This logic implies that in order for the array to remain monotonic, isNonIncreasing and isNonDecreasing must either both be true or one of the two is true in
+			// order for the method to return true. If it is increasing at one point but decreasing at another, the array cannot be monotonic. In order to return this logic...
+			
 		}
 		
+		// 5. We return an 'or' expression, which only returns false we both variables equal false. Any other combination will return true.
 		return isNonIncreasing || isNonDecreasing;
 	}
 

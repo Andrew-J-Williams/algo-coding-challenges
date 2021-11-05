ac39f46d97313693cd162c9389d371cc0f8a172f
Andrew Williams <1williamsaj@gmail.com>
2021-11-05 09:01:24 -0500

    Added comment prompt to MoveElementToEnd

--- src/com/challenges/medium/MoveElementToEnd.java
+++ src/com/challenges/medium/MoveElementToEnd.java
@@ -2,6 +2,16 @@ package com.challenges.medium;
 
 import java.util.*;
 
+/*
+ * 
+	Move Element To End
+	
+	You're given an array of integers and an integer. Write a function that moves all instances of that integer in the array to the end of the array and returns the array.
+	
+	The function should perform in place (i.e., it should mutate the input array) and doesn't need to maintain the order of the other integers.
+ * 
+ */
+
 public class MoveElementToEnd {
 
 	public static List<Integer> moveElementToEnd(List<Integer> array, int toMove) {

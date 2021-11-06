961bfe99390e2d4d8353747a83edb2e656e8ef8e
Andrew Williams <1williamsaj@gmail.com>
2021-11-06 08:38:06 -0500

    Added initial solution code for Spiral Traverse

--- src/com/challenges/medium/SpiralTraverse.java
+++ src/com/challenges/medium/SpiralTraverse.java
@@ -10,15 +10,53 @@ import java.util.*;
 	order.
 	
 	Spiral order starts at the top left corner of the two dimensional array, goes to the right, and proceeds in a spiral pattern all the way until every element has been visited.
+	
+	Sample Input:
+	
+	array = [
+		[ 1, 2, 3, 4 ],
+		[12, 13, 14, 5],
+		[11, 16, 15, 6],
+		[10, 9, 8, 7]
+	]
+	
+	Sample Output:
+	
+	[ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16 ]
  * 
  */
 
 public class SpiralTraverse {
 	
 	public static List<Integer> spiralTraverse(int[][] array) {
-	    // Write your code here.
-	    return new ArrayList<Integer>();
+
+		List<Integer> finalArray = new ArrayList<>();
+		
+		int size = array.length - 1;
+		
+		int firstRow = 0;
+		int firstColumn = 0;
+		int endingRow = 3;
+		int endingColumn = 3;
+		
+		while(firstRow < endingRow && firstColumn < endingColumn) {
+			
+			
+			firstRow++;
+			endingRow--;
+		}
+		
+		
+	    return finalArray;
 	  }
+	
+	public static void addNumbers(int[] array) {
+		
+		for(int i = 0; i < array.length; i++) {
+			
+		}
+		
+	}
 
 	public static void main(String[] args) {
 		int[][] array1 = { {1, 2, 3, 4}, {12, 13, 14, 5}, {11, 16, 15, 6}, {10, 9, 8, 7} };

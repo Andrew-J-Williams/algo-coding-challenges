a7be6d508d2a959057d1aca475134514d6e4d82b
Andrew Williams <1williamsaj@gmail.com>
2021-11-08 15:07:59 -0600

    Found solution for LongestPeak.

--- src/com/challenges/medium/LongestPeak.java
+++ src/com/challenges/medium/LongestPeak.java
@@ -23,13 +23,49 @@ package com.challenges.medium;
 public class LongestPeak {
 
 	public static int longestPeak(int[] array) {
-	    // Write your code here.
-	    return -1;
+		
+		int length = 0;
+		int i = 1;
+		
+		while(i < array.length - 1) {
+		
+			boolean isPeak = array[i - 1] < array[i] && array[i] > array[i + 1];
+			
+			if(!isPeak) {
+				i += 1;
+				continue;
+			}
+			
+			int leftIndex = i - 2;
+			
+			while(leftIndex >= 0 && array[leftIndex] < array[leftIndex + 1]) {
+				leftIndex -= 1;
+			}
+			
+			int rightIndex = i + 2;
+			
+			while(rightIndex < array.length && array[rightIndex] < array[rightIndex - 1]) {
+				rightIndex += 1;
+			}
+			
+			int currentPeak = rightIndex - leftIndex - 1;
+			
+			if(currentPeak > length) {
+				length = currentPeak;
+			}
+			
+			i = rightIndex;
+			
+		}
+		
+		
+	    return length;
 	  }
 	
 	public static void main(String[] args) {
 		
 		int[] array1 = { 1, 2, 3, 3, 4, 0, 10, 6, 5, -1, -3, 2, 3 };
+		int[] array2 = { 1, 3, 2 };
 		
 		System.out.println(longestPeak(array1));
 

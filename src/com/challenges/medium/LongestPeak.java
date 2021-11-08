ef7a7456b86198321d149e2bab9fe08f6d79dc70
Andrew Williams <1williamsaj@gmail.com>
2021-11-08 10:33:44 -0600

    Added prompt code to LongestPeak

--- src/com/challenges/medium/LongestPeak.java
+++ src/com/challenges/medium/LongestPeak.java
@@ -2,6 +2,11 @@ package com.challenges.medium;
 
 public class LongestPeak {
 
+	public static int longestPeak(int[] array) {
+	    // Write your code here.
+	    return -1;
+	  }
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 

ad4463976d3b3b0ba774ea04b30e8dca9a697519
Andrew Williams <1williamsaj@gmail.com>
2021-11-08 09:22:23 -0600

    In the processing of adding comment logic to SpiralTraverse

--- src/com/challenges/medium/SpiralTraverse.java
+++ src/com/challenges/medium/SpiralTraverse.java
@@ -30,38 +30,68 @@ public class SpiralTraverse {
 	
 	public static List<Integer> spiralTraverse(int[][] array) {
 
+		// 1. We create an ArrayList that will hold the values of our two dimensional array in order.
 		List<Integer> finalArray = new ArrayList<>();
-			
-		int firstRow = 0;
-		int firstColumn = 0;
-		int endingRow = array.length - 1;
-		int endingColumn = array[0].length - 1;
 		
+		// 2. We create 4 variables to represent the starting and ending rows and columns. We will traverse the array as if we were traversing the outside of a square, working our
+		// way towards the center in a spiral-like fashion. Right -> Down -> Left -> Up (Repeat, ignoring any values that were already added). 
+		
+		int firstRow = 0; // 1, 2, 3, 4
+		int firstColumn = 0; // 1, 12, 11, 10
+		int endingRow = array.length - 1; // 10, 9, 8, 7
+		int endingColumn = array[0].length - 1; // 4, 5, 6, 7 (we get this by checking the first array within the two dimensional array and grabbing the last value)
+		
+		// 3. We create a 'while' loop that runs until either the first row or first column's value exceeds the ending row or column's. This way we ensure our traversal stays
+		// within the bounds of our imaginary square and we account for all values present.
 		while(firstRow <= endingRow && firstColumn <= endingColumn) {
 			
+			// 4. We run 4 'for' loops, each responsible for traversing one side of the square. The first one goes across the starting row, running until the value of the 'i' is
+			// greater than the ending column's value. In this case, 'i' is equal to the value of the starting column.
 			for(int i = firstColumn; i <= endingColumn; i++) {
+				
+				// 5. We add every value into our ArrayList, covering every value in the starting row. Since this is a two dimensional array, the first value is our first row's value
+				// followed by the value of 'i', representing the index of each value in the sub-array. i.e. [ 1, 2, 3, 4 ].
 				finalArray.add(array[firstRow][i]);
 			}
 			
+			// 6. The 2nd 'for' loop, 'j' equals the starting row's value and the loop runs until 'j' is greater than the ending row's value.
 			for(int j = firstRow; j <= endingRow; j++) {
+				
+				// 7. We add the last value of each sub-array (row) to our ArrayList, with 'j' being the increasing value and the ending column being the last value.
+				// i.e. [ 1, 2, 3, 4, 5, 6, 7 ]
 				finalArray.add(array[j][endingColumn]);
 			}
 			
+			// 8. For the 3rd 'for' loop, we set 'k' equal to the value of our ending column minus 1 (since we have already included the last value of the last row in our ArrayList)
+			// and run it until 'k' is less than the value of the first column.
 			for(int k = endingColumn - 1; k >= firstColumn; k--) {
 				
+				// 9. In order to account for an edge case, meaning when there's a single row in the middle of the matrix, we include a conditional that checks to see if the first row
+				// is equal to the ending row. We do this to avoid double counting values. If it does...
 				if(firstRow == endingRow) {
+					
+					// ...we break our 'for' loop and continue to the next step below.
 					break;
 				}
 				
+				// 10. Otherwise, we add in every value from our ending row from right to left, where the ending row's value is the first parameter in our two dimensional array and 'k'
+				// is the decrementing value of each number in that row. i.e. [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ].
 				finalArray.add(array[endingRow][k]);
 			}
 			
+			// 11. Our 4th and final 'for' loop runs until 'l', which equals the ending row's value minus 1 (since we've already included the first value of the ending row), is less
+			// than or equal to the starting row's value.
 			for(int l = endingRow - 1; l > firstRow; l--) {
 				
+				// 12. As with the previous loop, we check for edge cases where there's a single column in the middle of the matrix by seeing if the starting column's value equals
+				// the ending column's value. If it does...
 				if(firstColumn == endingColumn) {
+					
+					// ...we break the loop and continue to the next step in our 'while' loop.
 					break;
 				}
 				
+				// 13. Otherwise, we add the first value of every row to our ArrayList.
 				finalArray.add(array[l][firstColumn]);
 			}
 			

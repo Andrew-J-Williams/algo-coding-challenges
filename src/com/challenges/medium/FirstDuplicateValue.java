4f7fbbed112ec8b2cfee8456c04475b5a13dada3
Andrew Williams <1williamsaj@gmail.com>
2021-11-08 16:09:42 -0600

    Added comment logic to FirstDuplicateValue

--- src/com/challenges/medium/FirstDuplicateValue.java
+++ src/com/challenges/medium/FirstDuplicateValue.java
@@ -21,17 +21,24 @@ public class FirstDuplicateValue {
 	
 	public static int firstDuplicateValue(int[] array) {
 	    
+		// 1. We create a HashSet for the purpose of identifying unique values. A HashSet will NOT accept duplicate values, only one unique instance of each integer.
 		HashSet<Integer> values = new HashSet<>();
 		
+		// 2. We create a 'for' loop that iterates over every element 'x' in our integer array, covering every single value.
 		for(int x : array) {
+			
+			// 3. We set up a conditional that checks if our HashSet doesn't contain the current value, if it doesn't...
 			if(!values.contains(x)) {
+				// ...we add that value to the HashSet.
 				values.add(x);
+			// 4. Otherwise, if 'x' is present in the HashSet, we know it has to be a duplicate since it was previously added...	
 			} else {
+				// ...thus we return the value of x.
 				return x;
 			}
 		}
 		
-		
+		// 5. If the loop completes, it means that there were no duplicate values in the array, thus we return -1 to indicate this fact.
 	    return -1;
 	  }
 

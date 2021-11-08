97b495f1491512a3d8876997edb3407cdc432227
Andrew Williams <1williamsaj@gmail.com>
2021-11-08 15:53:43 -0600

    Added prompt code to FirstDuplicateValue

--- src/com/challenges/medium/FirstDuplicateValue.java
+++ src/com/challenges/medium/FirstDuplicateValue.java
@@ -1,6 +1,11 @@
 package com.challenges.medium;
 
 public class FirstDuplicateValue {
+	
+	public static int firstDuplicateValue(int[] array) {
+	    // Write your code here.
+	    return -1;
+	  }
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub

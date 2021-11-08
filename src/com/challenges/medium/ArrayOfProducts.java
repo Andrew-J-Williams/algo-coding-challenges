dcc585355c89a432510eb5072422a4ab619e5b7d
Andrew Williams <1williamsaj@gmail.com>
2021-11-08 15:10:58 -0600

    Added prompt code to ArrayOfProducts

--- src/com/challenges/medium/ArrayOfProducts.java
+++ src/com/challenges/medium/ArrayOfProducts.java
@@ -2,6 +2,11 @@ package com.challenges.medium;
 
 public class ArrayOfProducts {
 
+	public int[] arrayOfProducts(int[] array) {
+		// Write your code here.
+		return new int[] {};
+	}
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 

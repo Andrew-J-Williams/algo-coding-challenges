22f777ee1a93fae919ab504ee8d9f77e9d23da4e
Andrew Williams <1williamsaj@gmail.com>
2021-11-08 16:17:57 -0600

    Added prompt code to MergeOverlappingIntervals

--- src/com/challenges/medium/MergeOverlappingIntervals.java
+++ src/com/challenges/medium/MergeOverlappingIntervals.java
@@ -1,6 +1,13 @@
 package com.challenges.medium;
 
+
+
 public class MergeOverlappingIntervals {
+
+	public int[][] mergeOverlappingIntervals(int[][] intervals) {
+		// Write your code here.
+		return new int[][] {};
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub

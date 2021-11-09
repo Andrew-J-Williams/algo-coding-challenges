26b99f27bed9eba3d289c53e081847bdd55a5b8f
Andrew Williams <1williamsaj@gmail.com>
2021-11-09 10:47:04 -0600

    Added prompt code to BTSConstruction

--- src/com/challenges/medium/BTSConstruction.java
+++ src/com/challenges/medium/BTSConstruction.java
@@ -2,9 +2,36 @@ package com.challenges.medium;
 
 public class BTSConstruction {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	static class BST {
+		public int value;
+		public BST left;
+		public BST right;
+
+		public BST(int value) {
+			this.value = value;
+		}
+
+		public BST insert(int value) {
+			// Write your code here.
+			// Do not edit the return statement of this method.
+			return this;
+		}
+
+		public boolean contains(int value) {
+			// Write your code here.
+			return false;
+		}
+
+		public BST remove(int value) {
+			// Write your code here.
+			// Do not edit the return statement of this method.
+			return this;
+		}
+
+		public static void main(String[] args) {
+			// TODO Auto-generated method stub
+
+		}
 
 	}
-
 }

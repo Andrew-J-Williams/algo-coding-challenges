d7e8584700d01e2e7f3e370d23f6c13e02bd9679
Andrew Williams <1williamsaj@gmail.com>
2021-11-10 09:25:26 -0600

    Added prompt code to ValidateBST

--- src/com/challenges/medium/ValidateBST.java
+++ src/com/challenges/medium/ValidateBST.java
@@ -2,6 +2,21 @@ package com.challenges.medium;
 
 public class ValidateBST {
 
+	public static boolean validateBst(BST tree) {
+		// Write your code here.
+		return false;
+	}
+
+	static class BST {
+		public int value;
+		public BST left;
+		public BST right;
+
+		public BST(int value) {
+			this.value = value;
+		}
+	}
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 

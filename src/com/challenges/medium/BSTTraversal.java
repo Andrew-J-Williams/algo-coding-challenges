a0fb4da8362fc3c7c92be6fca14d98e84747acda
Andrew Williams <1williamsaj@gmail.com>
2021-11-10 12:53:25 -0600

    Added comment prompt to BSTTraversal

--- src/com/challenges/medium/BSTTraversal.java
+++ src/com/challenges/medium/BSTTraversal.java
@@ -1,5 +1,20 @@
 package com.challenges.medium;
 
+import java.util.*;
+
+/*
+ * 
+	BST Traversal
+	
+	Write three functions that take in a Binary Search Tree (BST) and an empty array, traverse that BST, add its nodes' values to the input array, and return that array. The three
+	functions should traverse the BST using the in-order, pre-order, and post-order tree-traversal techniques, respectively.
+	
+	Each BST node has an integer 'value', a 'left' child node, and a 'right' child node. A node is said to be a valid BST node if and only if it satisfies the BST property: its value is 
+	strictly greater than the values of every node to its left; its value is less than or equal to the values of every node to its right; and its children nodes are either valid BST
+	nodes themselves or None/null. 
+ * 
+ */
+
 public class BSTTraversal {
 
 	public static List<Integer> inOrderTraverse(BST tree, List<Integer> array) {

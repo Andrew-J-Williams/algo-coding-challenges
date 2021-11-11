fb1d1f0c56c4e41da9384b32c48bead6e66186df
Andrew Williams <1williamsaj@gmail.com>
2021-11-11 09:35:26 -0600

    Added comment prompt to MinHeightBST

--- src/com/challenges/medium/MinHeightBST.java
+++ src/com/challenges/medium/MinHeightBST.java
@@ -3,11 +3,34 @@ package com.challenges.medium;
 import java.util.*;
 
 
+/*
+ * 
+	Min Height BST
+	
+	Write a function that takes in a non-empty sorted array of distinct integers, construct a BST from the integers, and returns the root of the BST.
+	
+	The function should minimize the height of the BST.
+	
+	You've been provided with a BST class that you'll use to construct the BST.
+	
+	Each BST node has an integer 'value', a 'left' child node, and a 'right' child node. A node is said to be a valid BST node if and only if it satisfies the BST property: its value is 
+	strictly greater than the values of every node to its left; its value is less than or equal to the values of every node to its right; and its children nodes are either valid BST
+	nodes themselves or None/null. 
+	
+	A BST is valid if and only if all of its nodes are valid BST nodes.
+	
+	Note that the BST class has an 'insert' method you can use if you want.
+ * 
+ */
+
 
 public class MinHeightBST {
 
 	public static BST minHeightBst(List<Integer> array) {
-		// Write your code here.
+		
+		
+		
+		
 		return null;
 	}
 

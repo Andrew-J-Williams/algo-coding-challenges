222098e1dcf6cd3ebd0abd5cff866d0119a4c77c
Andrew Williams <1williamsaj@gmail.com>
2021-11-11 12:08:35 -0600

    Added prompt code for KthLargestValueBST

--- src/com/challenges/medium/KthLargestValueBST.java
+++ src/com/challenges/medium/KthLargestValueBST.java
@@ -2,6 +2,21 @@ package com.challenges.medium;
 
 public class KthLargestValueBST {
 
+	static class BST {
+		public int value;
+		public BST left = null;
+		public BST right = null;
+
+		public BST(int value) {
+			this.value = value;
+		}
+	}
+
+	public int findKthLargestValueInBst(BST tree, int k) {
+		// Write your code here.
+		return -1;
+	}
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 

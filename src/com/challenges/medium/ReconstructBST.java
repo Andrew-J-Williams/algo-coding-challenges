6c9f00d470ecf6705b11d0fc23d9b9ff0f650a1a
Andrew Williams <1williamsaj@gmail.com>
2021-11-11 14:11:28 -0600

    Added prompt code and necessary imports to ReconstructBST

--- src/com/challenges/medium/ReconstructBST.java
+++ src/com/challenges/medium/ReconstructBST.java
@@ -1,6 +1,23 @@
 package com.challenges.medium;
 
+import java.util.*;
+
 public class ReconstructBST {
+	
+	static class BST {
+	    public int value;
+	    public BST left = null;
+	    public BST right = null;
+
+	    public BST(int value) {
+	      this.value = value;
+	    }
+	  }
+
+	  public BST reconstructBst(ArrayList<Integer> preOrderTraversalValues) {
+	    // Write your code here.
+	    return null;
+	  }
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub

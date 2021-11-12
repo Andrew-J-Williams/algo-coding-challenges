57ab77b3b4268f9569939f0a43e179c8d1e94fee
Andrew Williams <1williamsaj@gmail.com>
2021-11-12 14:34:18 -0600

    Added test data to HeightBalancedBinaryTree

--- src/com/challenges/medium/HeightBalancedBinaryTree.java
+++ src/com/challenges/medium/HeightBalancedBinaryTree.java
@@ -12,13 +12,31 @@ public class HeightBalancedBinaryTree {
 		}
 	}
 
-	public boolean heightBalancedBinaryTree(BinaryTree tree) {
+	public static boolean heightBalancedBinaryTree(BinaryTree tree) {
 		// Write your code here.
 		return false;
 	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		BinaryTree root = new BinaryTree(1);
+		BinaryTree n1 = new BinaryTree(2);
+		BinaryTree n2 = new BinaryTree(3);
+		BinaryTree n3 = new BinaryTree(4);
+		BinaryTree n4 = new BinaryTree(5);
+		BinaryTree n5 = new BinaryTree(6);
+		BinaryTree n6 = new BinaryTree(7);
+		BinaryTree n7 = new BinaryTree(8);
+		
+		root.left = n1;
+		root.right = n2;
+		root.left.left = n3;
+		root.left.right = n4;
+		root.right.right = n5;
+		root.left.right.left = n6;
+		root.left.right.right = n7;
 
+		
+		System.out.println(heightBalancedBinaryTree(root));
 	}
 }

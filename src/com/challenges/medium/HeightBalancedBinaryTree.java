77e22e8d988d66e6a90d3c9115cd1815a6922ff1
Andrew Williams <1williamsaj@gmail.com>
2021-11-12 14:38:24 -0600

    Added comment prompt to HeightBalancedBinaryTree

--- src/com/challenges/medium/HeightBalancedBinaryTree.java
+++ src/com/challenges/medium/HeightBalancedBinaryTree.java
@@ -1,5 +1,18 @@
 package com.challenges.medium;
 
+/*
+ * 
+ 	Height Balanced Binary Tree
+ 	
+	You're given the root node of a Binary Tree. Write a function that returns 'true' if this Binary Tree is height balanced and 'false' if it isn't.
+	
+	A Binary Tree is height balanced if for each node in the tree, the difference between the height of its left subtree and the height of its source subtree is at most 1.
+	
+	Each BinaryTree has an integer value, a left child node, and a right child node. Children nodes can either be BinaryTree nodes themselves or None/null.
+	 
+ * 
+ */
+
 public class HeightBalancedBinaryTree {
 
 	static class BinaryTree {

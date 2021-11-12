a45c5908b73498459f92ee5adb0ebc5c889d2a5a
Andrew Williams <1williamsaj@gmail.com>
2021-11-11 18:40:08 -0600

    Added prompt code to InvertBinaryTree

--- src/com/challenges/medium/InvertBinaryTree.java
+++ src/com/challenges/medium/InvertBinaryTree.java
@@ -2,6 +2,20 @@ package com.challenges.medium;
 
 public class InvertBinaryTree {
 
+	public static void invertBinaryTree(BinaryTree tree) {
+		// Write your code here.
+	}
+
+	static class BinaryTree {
+		public int value;
+		public BinaryTree left;
+		public BinaryTree right;
+
+		public BinaryTree(int value) {
+			this.value = value;
+		}
+	}
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 

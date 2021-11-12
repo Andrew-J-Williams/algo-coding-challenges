3b9f6c91207af2db33897fe0bb0c19713068167b
Andrew Williams <1williamsaj@gmail.com>
2021-11-12 10:48:38 -0600

    Added prompt code to FindSuccesor

--- src/com/challenges/medium/FindSuccessor.java
+++ src/com/challenges/medium/FindSuccessor.java
@@ -2,6 +2,22 @@ package com.challenges.medium;
 
 public class FindSuccessor {
 
+	static class BinaryTree {
+		public int value;
+		public BinaryTree left = null;
+		public BinaryTree right = null;
+		public BinaryTree parent = null;
+
+		public BinaryTree(int value) {
+			this.value = value;
+		}
+	}
+
+	public BinaryTree findSuccessor(BinaryTree tree, BinaryTree node) {
+		// Write your code here.
+		return null;
+	}
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 

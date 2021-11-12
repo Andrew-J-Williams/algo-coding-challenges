e4b6b98a33776aca9eefb4d7315c64c6e9c3f1a6
Andrew Williams <1williamsaj@gmail.com>
2021-11-11 19:22:03 -0600

    Added prompt code to BinaryTreeDiameter

--- src/com/challenges/medium/BinaryTreeDiameter.java
+++ src/com/challenges/medium/BinaryTreeDiameter.java
@@ -2,6 +2,21 @@ package com.challenges.medium;
 
 public class BinaryTreeDiameter {
 
+	static class BinaryTree {
+		public int value;
+		public BinaryTree left = null;
+		public BinaryTree right = null;
+
+		public BinaryTree(int value) {
+			this.value = value;
+		}
+	}
+
+	public int binaryTreeDiameter(BinaryTree tree) {
+		// Write your code here.
+		return -1;
+	}
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 

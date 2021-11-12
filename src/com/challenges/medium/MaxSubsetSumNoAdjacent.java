3499ff1435b14806234dd0575f18c14b8a9d0958
Andrew Williams <1williamsaj@gmail.com>
2021-11-12 16:50:16 -0600

    Added prompt code to MaxSubsetSumNoAdjacent

--- src/com/challenges/medium/MaxSubsetSumNoAdjacent.java
+++ src/com/challenges/medium/MaxSubsetSumNoAdjacent.java
@@ -2,6 +2,11 @@ package com.challenges.medium;
 
 public class MaxSubsetSumNoAdjacent {
 
+	public static int maxSubsetSumNoAdjacent(int[] array) {
+		// Write your code here.
+		return -1;
+	}
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 

0ee5c1298272f67a9f040946e47217d3f4b2d29a
Andrew Williams <1williamsaj@gmail.com>
2021-11-11 18:38:41 -0600

    Added comment logic to ReconstructBST

--- src/com/challenges/medium/ReconstructBST.java
+++ src/com/challenges/medium/ReconstructBST.java
@@ -55,28 +55,52 @@ public class ReconstructBST {
 			return null;
 		}
 		
+		// 2. We create an integer variable that holds the value of the first number in our ArrayList.
 		int current = preOrderTraversalValues.get(0);
+		
+		// 3. We create another integer variable that is equal to the size of our ArrayList.
 		int rightSubtreeRootIdx = preOrderTraversalValues.size();
 		
+		// 4. We create a 'for' loop that starts at the second value of the ArrayList and iterates until 'i' is greater than or equal to the size of the ArrayList. We start at the 
+		// second element because we will be comparing every element to the first.
 		for(int i = 1; i < preOrderTraversalValues.size(); i++) {
 			
+			// 5. We create an integer variable that holds the value of the current element at index 'i'.
 			int value = preOrderTraversalValues.get(i);
 			
+			// 6. We check if the value at index 'i' is greater than or equal to the current value. We know that our current value will be the root node's value, so if the value at
+			// index 'i' is greater or equal to current, we know it will be its right child node because in a valid BST, all values greater than or equal to the parent node's value
+			// will be in the right subtree. If this is the case...
 			if(value >= current) {
+				
+				// ...we set our right subtree's root index equal to index value 'i'.
 				rightSubtreeRootIdx = i;
+				
+				// 7. We break the 'for' loop, stopping any further iteration. We do this because once we've found what the right child node's value is, we know all values that come
+				// before it in our ArrayList are less than both the root node's value and the right child node's value. Knowing this, we can split our original ArrayList into 2 new
+				// ArrayLists: one for our left subtree and one for our right subtree. 
 				break;
 			}
 		}
 		
+		// 8. We create a BST node and set it equal to our function. We pass in a subList of our original ArrayList that starts at index 1 and ends at the rightSubtreeRootIdx. Given our
+		// test data, this would produce the following list: [ 4, 2, 1, 5 ]. Remember, the ending index value is not captured in the range. Since all these values are less than the 
+		// root node (10), they will comprise our left subtree. The logic repeats until the size of our List passed in equals 0, and it eventually will as the List of numbers grows
+		// smaller with each recursive call.
 		BST leftSubtree = reconstructBst(preOrderTraversalValues.subList(1, rightSubtreeRootIdx));
 		
+		// 9. We create another BST node and set it equal to our function. We pass in a subList of our original ArrayList that starts at index 1 the rightSubtreeRootIdx and ends at the
+		// size of our List. Given our test data, this would produce the following list: [ 17, 19, 18 ].
 		BST rightSubtree = reconstructBst(preOrderTraversalValues.subList(rightSubtreeRootIdx, preOrderTraversalValues.size()));
 		
+		// 10. We create one final BST that will represent our root/parent node and we set the value equal to our current value (the first element from our ArrayList).
 		BST bst = new BST(current);
 		
+		// 11. We set the left and right child nodes equal to our left and right subtrees.
 		bst.left = leftSubtree;
 		bst.right = rightSubtree;
 		
+		// 12. Finally, we return our root node.
 		return bst;
 	}
 	

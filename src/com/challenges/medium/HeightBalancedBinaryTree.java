6b22d046764372a9260000f6667a318916fcb0e4
Andrew Williams <1williamsaj@gmail.com>
2021-11-12 14:24:14 -0600

    Added prompt code for HeightBalancedBinaryTree

--- src/com/challenges/medium/HeightBalancedBinaryTree.java
+++ src/com/challenges/medium/HeightBalancedBinaryTree.java
@@ -2,9 +2,23 @@ package com.challenges.medium;
 
 public class HeightBalancedBinaryTree {
 
+	static class BinaryTree {
+		public int value;
+		public BinaryTree left = null;
+		public BinaryTree right = null;
+
+		public BinaryTree(int value) {
+			this.value = value;
+		}
+	}
+
+	public boolean heightBalancedBinaryTree(BinaryTree tree) {
+		// Write your code here.
+		return false;
+	}
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
 	}
-
 }

082e84774b37ed4926d3fb6e1340f9533b158681
Andrew Williams <1williamsaj@gmail.com>
2021-11-13 09:11:07 -0600

    Added comment prompt to MaxSubsetSumNoAdjacent

--- src/com/challenges/medium/MaxSubsetSumNoAdjacent.java
+++ src/com/challenges/medium/MaxSubsetSumNoAdjacent.java
@@ -1,5 +1,22 @@
 package com.challenges.medium;
 
+/*
+ * 
+	Max Subset Sum No Adjacent
+	
+	Write a function that takes in an array of positive integers and returns the maximum sum of non-adjacent elements in the array.
+	
+	If the input array is empty, the function should return 0.
+	
+	
+	Sample Input:
+	array = [ 75, 105, 120, 75, 90, 135 ]
+	
+	Sample Output:
+	330
+ * 
+ */
+
 public class MaxSubsetSumNoAdjacent {
 
 	public static int maxSubsetSumNoAdjacent(int[] array) {

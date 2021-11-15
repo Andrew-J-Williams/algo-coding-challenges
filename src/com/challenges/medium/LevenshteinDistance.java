f9e63a19a9f1dfc7c97f088fb3da305d0abe2f7f
Andrew Williams <1williamsaj@gmail.com>
2021-11-15 15:05:07 -0600

    Added prompt code to LevenshteinDistance

--- src/com/challenges/medium/LevenshteinDistance.java
+++ src/com/challenges/medium/LevenshteinDistance.java
@@ -2,6 +2,11 @@ package com.challenges.medium;
 
 public class LevenshteinDistance {
 
+	public static int levenshteinDistance(String str1, String str2) {
+		// Write your code here.
+		return -1;
+	}
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 

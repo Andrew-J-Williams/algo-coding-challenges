7e655a29af66c9769dd1c11c612454f4debbf0ac
Andrew Williams <1williamsaj@gmail.com>
2021-11-15 15:29:58 -0600

    Added comment prompt to LevenshteinDistance

--- src/com/challenges/medium/LevenshteinDistance.java
+++ src/com/challenges/medium/LevenshteinDistance.java
@@ -1,9 +1,30 @@
 package com.challenges.medium;
 
+/*
+ * 
+	Levenshtein Distance
+	
+	Write a function that takes in two strings and returns the minimum number of edit operations that need to be performed on the first string to obtain the second string.
+	
+	There are 3 different edit operations: insertion of a character, deletion of a character, and the substitution of a character for another.
+	
+	Sample Input:
+	str1 = "abc"
+	str2 = "yabd" 
+	
+	Sample Output:
+	2 // insert "y"; substitute "c" for "d"
+ * 
+ */
+
 public class LevenshteinDistance {
 
 	public static int levenshteinDistance(String str1, String str2) {
-		// Write your code here.
+		
+		
+		
+		
+		
 		return -1;
 	}
 

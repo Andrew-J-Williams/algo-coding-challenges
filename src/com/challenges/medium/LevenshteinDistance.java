8ee4a40282bc98d71cc1a6023e489904144e5ac5
Andrew Williams <1williamsaj@gmail.com>
2021-11-16 14:37:42 -0600

    Finished comment logic for LevenshteinDistance

--- src/com/challenges/medium/LevenshteinDistance.java
+++ src/com/challenges/medium/LevenshteinDistance.java
@@ -23,7 +23,7 @@ public class LevenshteinDistance {
 
 	public static int levenshteinDistance(String str1, String str2) {
 		
-		// 1. We created a two dimensional array with our 2nd string's length plus 1 representing the rows and our 1st string's length plus 1 representing our columns.
+		// 1. We created a two dimensional array with our 2nd string's length plus 1 representing the rows (i) and our 1st string's length plus 1 representing our columns (j).
 		int[][] edits = new int[str2.length() + 1][str1.length() + 1];
 		
 		// 2. We run two 'for' loops, the first runs the length of our 2nd string plus 1 and the second runs the length of our first string plus 1.
@@ -53,25 +53,61 @@ public class LevenshteinDistance {
 		}
 	
 		
-		// 5. We create another set of double 'for' loops, but the only difference this time is we set the initial values of 'i' and 'j' to 1 instead of 0.
+		// 5. We create another set of double 'for' loops, but the only difference this time is we set the initial values of 'i' and 'j' to 1 instead of 0. We do this to account
+		// for the fact that our 2D array has an index of 0 at edits[0][0] representing an empty string.
 		for(int i = 1; i < str2.length() + 1; i++) {
 			for(int j = 1; j < str1.length() + 1; j++) {
 				
+				// 6. If the character at the index of i - 1 in our second string is equal to the index of j - 1 in our first string...
 				if(str2.charAt(i - 1) == str1.charAt(j - 1)) {
+					
+					// ...then we update the value at edits[i][j] to the value found at edits[i - 1][j - 1]. To understand what is happening, lets use our example data to see how
+					// this works. At str2.charAt(2 - 1) and str1.charAt(1 - 1), both are equal to 'a'. So for this case, i = 2 and j = 1, so we would take edits[2][1] (whose value
+					// is 1) and set its value equal to the value found at edits[1][0], which just so happens to be 1, the same value. Basically, whenever the characters area equal
+					// we set edits[i][j] equal to the value at the left diagonal from it.
 					edits[i][j] = edits[i - 1][j - 1];
+					
+				// 7. If the characters are not equal (which happens to be the majority of the time for our example)...	
 				} else {
+					
+					// ...we set edits[i][j]'s value equal to the minimum value of 3 different values plus 1. These 3 different values represent the 3 values that surround the 
+					// current value: the value immediately left (edits[i][j - 1], the value at the left diagonal (edits[i - 1][j - 1], and the value immediately above the current
+					// value (edits[i - 1][j]). We add 1 because given are algorithm, when keeping track of edits, the number of edits is always equal to the minimum value of the
+					// 3 surrounding values plus 1. So given our example data, when i = 4 and j = 1, string 1's char is 'a' and string 2's char is 'd'. As a result, our function
+					// checks the 3 values surrounding edits[4][1] (4, 3, 2) and determines that 2 is the minimum overall. It then adds 1 to 2 and makes 3 value the new
+					// value of edits[4][1].
 					edits[i][j] = 1 + Math.min(edits[i - 1][j - 1], Math.min(edits[i - 1][j], edits[i][j - 1]));
 				}
 				
 			}
 		}
-				
+			// Below is the resulting matrix after these two 'for' loops finish.
+			/*
+		 
+		  	  j0 j1 j2 j3
+		  
+	    i0	 [ 0, 1, 2, 3 ]
+	    i1	 [ 1, 1, 2, 3 ]
+	    i2	 [ 2, 1, 2, 3 ]
+	    i3	 [ 3, 2, 1, 2 ]
+	    i4	 [ 4, 3, 2, 2 ]
+	 
+		 */
+		
+		System.out.println(Arrays.toString(edits[0]));
+		System.out.println(Arrays.toString(edits[1]));
+		System.out.println(Arrays.toString(edits[2]));
+		System.out.println(Arrays.toString(edits[3]));
+		System.out.println(Arrays.toString(edits[4]));
+		
+		
+		// 8. Finally, we return the final value in our two dimensional array, which can be found in the last value of the last row.
 		return edits[str2.length()][str1.length()];
 	}
 
 	public static void main(String[] args) {
-		String str1 = "abc";
-		String str2 = "yabd";
+		String str1 = "abc"; // j
+		String str2 = "yabd"; // i
 		
 		System.out.println(levenshteinDistance(str1, str2));
 

04040ae5560db5e4f930d2699d75a4d39a797d73
Andrew Williams <1williamsaj@gmail.com>
2021-11-16 12:57:44 -0600

    Found one solution to LevenshteinDistance, attempting to find one that passes all tests

--- src/com/challenges/medium/LevenshteinDistance.java
+++ src/com/challenges/medium/LevenshteinDistance.java
@@ -26,9 +26,45 @@ public class LevenshteinDistance {
 		String big = str1.length() >= str2.length() ? str1 : str2;
 		
 		
+		int[] evenEdits = new int[small.length() + 1];
+		int[] oddEdits = new int[small.length() + 1];
 		
+		for(int i = 0; i < small.length() + 1; i++) {
+			evenEdits[i] = i;
+		}
 		
-		return -1;
+		int[] currentEdits;
+		int[] previousEdits;
+		
+		for(int i = 1; i < big.length() + 1; i++) {
+			
+			
+			if(i % 2 == 0) {
+				currentEdits = oddEdits;
+				previousEdits = evenEdits;
+			} else {
+				currentEdits = evenEdits;
+				previousEdits = oddEdits;
+			}
+			
+			
+			currentEdits[0] = i;
+			
+			for(int j = 1; j < small.length() + 1; j++) {
+				
+				if(big.charAt(i - 1) == small.charAt(j-1)) {
+					currentEdits[j] = previousEdits[j - 1];
+				} else {
+					currentEdits[j] = 1 + Math.min(previousEdits[j -1], Math.min(previousEdits[j], currentEdits[j - 1]));
+				}
+				
+			}
+			
+			
+			
+		}
+		
+		return big.length() % 2 == 0 ? evenEdits[small.length()] : oddEdits[small.length()];
 	}
 
 	public static void main(String[] args) {

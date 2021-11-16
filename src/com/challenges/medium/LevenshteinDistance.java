4f4778b374746c877057666b9249cc271cebd7d9
Andrew Williams <1williamsaj@gmail.com>
2021-11-16 13:37:52 -0600

    Added 1/2 of comment logic to LevenshteinDistance

--- src/com/challenges/medium/LevenshteinDistance.java
+++ src/com/challenges/medium/LevenshteinDistance.java
@@ -1,5 +1,7 @@
 package com.challenges.medium;
 
+import java.util.Arrays;
+
 /*
  * 
 	Levenshtein Distance
@@ -22,20 +24,36 @@ public class LevenshteinDistance {
 	public static int levenshteinDistance(String str1, String str2) {
 		
 		// 1. We created a two dimensional array with our 2nd string's length plus 1 representing the rows and our 1st string's length plus 1 representing our columns.
-		// We add 1 to account for blank spaces/empty strings.
 		int[][] edits = new int[str2.length() + 1][str1.length() + 1];
 		
+		// 2. We run two 'for' loops, the first runs the length of our 2nd string plus 1 and the second runs the length of our first string plus 1.
 		for(int i = 0; i < str2.length() + 1; i++) {
 			for(int j = 0; j < str1.length() + 1; j++) {
 				
+				// 3. Within our 2nd 'for' loop, we are setting the current column equal to the value of 'j'. We repeat this for each row 'i'.
 				edits[i][j] = j;
 				
 			}
 			
+			// 4. After each row is created with the values of 'j' representing the column, we set the first value of each row equal to i. As a result, given our test data,
+			// the following matrix would be created:
+			/*
+			 
+			  c1 c2 c3 c4
+			  
+		r1	 [ 0, 1, 2, 3 ]
+		r2	 [ 1, 1, 2, 3 ]
+		r3	 [ 2, 1, 2, 3 ]
+		r4	 [ 3, 1, 2, 3 ]
+		r5	 [ 4, 1, 2, 3 ]
+		 
+			 */
+			 // NOTE: the first value of each row becomes the current value of 'i' as the loop iterates.
 			edits[i][0] = i;
 		}
+	
 		
-		
+		// 5. We create another set of double 'for' loops, but the only difference this time is we set the initial values of 'i' and 'j' to 1 instead of 0.
 		for(int i = 1; i < str2.length() + 1; i++) {
 			for(int j = 1; j < str1.length() + 1; j++) {
 				

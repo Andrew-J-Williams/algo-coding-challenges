697b1a3a2d0aa729b1214da7978781a8bf394644
Andrew Williams <1williamsaj@gmail.com>
2021-11-16 14:42:27 -0600

    Added comment prompt to NumberOfWaysToTraverseGraph

--- src/com/challenges/medium/NumberOfWaysToTraverseGraph.java
+++ src/com/challenges/medium/NumberOfWaysToTraverseGraph.java
@@ -2,13 +2,16 @@ package com.challenges.medium;
 
 public class NumberOfWaysToTraverseGraph {
 
-	public int numberOfWaysToTraverseGraph(int width, int height) {
+	public static int numberOfWaysToTraverseGraph(int width, int height) {
 		// Write your code here.
 		return -1;
 	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int width = 4;
+		int height = 3;
+		
+		System.out.println(numberOfWaysToTraverseGraph(width, height));
 
 	}
 

47c0dfc474f150643edda429292cdec2ed6633e5
Andrew Williams <1williamsaj@gmail.com>
2021-11-16 15:06:44 -0600

    Added comment prompt to NumberOfWaysToTraverseGraph

--- src/com/challenges/medium/NumberOfWaysToTraverseGraph.java
+++ src/com/challenges/medium/NumberOfWaysToTraverseGraph.java
@@ -1,5 +1,36 @@
 package com.challenges.medium;
 
+
+/*
+ * 
+	Number Of Ways To Traverse Graph
+	
+	You're given two positive integers representing the width and height of a grid-shaped, rectangular graph. Write a function that returns the number of ways to reach the bottom
+	right corner of the graph when starting from the top left corner. Each move you take must either go down or right. In other words, you can never move up or left in the graph.
+	
+	For example, given the graph illustrated below, with width = 2 and height = 3, there are three ways to reach the bottom right corner when starting at the top left corner:
+	
+	 _ _
+	|_|_|
+	|_|_|
+	|_|_| 
+	
+	1. Down, Down, Right
+	2. Right, Down, Down
+	3. Down, Right, Down
+	
+	NOTE: You may assume that width * height >= 2. In other words, the graph will never be a 1x1 grid.
+	
+	Sample Input:
+	width = 4
+	height = 3
+	
+	Sample Output:
+	10
+ * 
+ */
+
+
 public class NumberOfWaysToTraverseGraph {
 
 	public static int numberOfWaysToTraverseGraph(int width, int height) {
@@ -8,6 +39,7 @@ public class NumberOfWaysToTraverseGraph {
 	}
 
 	public static void main(String[] args) {
+		
 		int width = 4;
 		int height = 3;
 		

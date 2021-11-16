6e5f0390b52bf3b046b5d360a69a6fd90f57620e
Andrew Williams <1williamsaj@gmail.com>
2021-11-16 14:39:35 -0600

    Added prompt code to NumberOfWaysToTraverseGraph

--- src/com/challenges/medium/NumberOfWaysToTraverseGraph.java
+++ src/com/challenges/medium/NumberOfWaysToTraverseGraph.java
@@ -2,6 +2,11 @@ package com.challenges.medium;
 
 public class NumberOfWaysToTraverseGraph {
 
+	public int numberOfWaysToTraverseGraph(int width, int height) {
+		// Write your code here.
+		return -1;
+	}
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 

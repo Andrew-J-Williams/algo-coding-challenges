3ffffe720a159ffda12e41e40e93ce4cf8e27c4f
Andrew Williams <1williamsaj@gmail.com>
2021-11-17 10:28:51 -0600

    Added prompt code to SingleCycleCheck

--- src/com/challenges/medium/SingleCycleCheck.java
+++ src/com/challenges/medium/SingleCycleCheck.java
@@ -2,6 +2,11 @@ package com.challenges.medium;
 
 public class SingleCycleCheck {
 
+	public static boolean hasSingleCycle(int[] array) {
+		// Write your code here.
+		return false;
+	}
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 

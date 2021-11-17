9d1875ac068a890fd9298bb3c4bfab3cb61c0da4
Andrew Williams <1williamsaj@gmail.com>
2021-11-17 14:43:02 -0600

    Added comment logic to breadthFirstSearch

--- src/com/challenges/medium/BreadthFirstSearch.java
+++ src/com/challenges/medium/BreadthFirstSearch.java
@@ -38,15 +38,29 @@ public class BreadthFirstSearch {
 
 		public List<String> breadthFirstSearch(List<String> array) {
 			
+			// 1. We create a queue that will hold various number of nodes as we traverse through our graph.
 			Queue<Node> queue = new LinkedList<Node>();
+			
+			// 2. We add the first node in our graph ("A") to the queue, making it non-empty.
 			queue.add(this);
 			
+			// 3. We create a 'while' loop that runs as long as our queue is not empty. Given the fact that our tree should always have a root node, this loop will execute at least
+			// once.
 			while(!queue.isEmpty()) {
+				
+				// 4. We create a node and set it equal to the first element we pull from the top of our queue. In the case of our example data, we would be removing "A".
 				Node current = queue.poll();
+				
+				// 5. We then add the name of this node to our ArrayList. Thus, with our example the ArrayList would look like: [ "A" ]. The queue would look like: [ ]. If our
+				// current node doesn't have any children, then nothing is added.
 				array.add(current.name);
+				
+				// 6. We add all the children from the current node (which would be an ArrayList of nodes) to our queue, preventing our queue from being empty and keeping the loop
+				// running until we reach the end of our graph. So before the next loop, our ArrayList would look like [ "A" ] and our queue would look like [ "B", "C", "D" ].
 				queue.addAll(current.children);
 			}
 			
+			// 7. Once our queue is empty, indicating all nodes in the graph have been traversed, we return our ArrayList of node names.
 			return array;
 		}
 

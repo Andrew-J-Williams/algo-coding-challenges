d93ed22c38fbfa9b8fed549ed5f2c6cf32402f10
Andrew Williams <1williamsaj@gmail.com>
2021-11-17 13:54:27 -0600

    Added comment prompt to BreadthFirstSearch

--- src/com/challenges/medium/BreadthFirstSearch.java
+++ src/com/challenges/medium/BreadthFirstSearch.java
@@ -2,6 +2,30 @@ package com.challenges.medium;
 
 import java.util.*;
 
+/*
+ * 
+	Breadth-first Search
+	
+	You're given a Node class that has a name and an array of optional children. When put together, nodes form an acyclic tree-like structure.
+	
+	Implement the breadthFirstSearch method on the Node class, which takes in an empty array, traverses the tree using the Breadth-first Search approach (specifically navigating
+	the tree from left to right), stores all of the nodes' names in the input array, and returns it.
+	
+	Sample Input:
+	
+		graph = A
+			  / | \	
+		     B  C  D
+		    / \   / \
+		   E   F G   H
+		      / \ \
+		     I   J K
+	
+	Sample Output:
+	[ "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K" ]
+ * 
+ */
+
 public class BreadthFirstSearch {
 	
 	static class Node {

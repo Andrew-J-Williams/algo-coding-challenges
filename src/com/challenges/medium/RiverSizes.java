974bbc0702382f4d5b6556fa8d3e650df10733e0
Andrew Williams <1williamsaj@gmail.com>
2021-11-17 14:44:53 -0600

    Added prompt code to RiverSizes

--- src/com/challenges/medium/RiverSizes.java
+++ src/com/challenges/medium/RiverSizes.java
@@ -1,6 +1,13 @@
 package com.challenges.medium;
 
+import java.util.*;
+
 public class RiverSizes {
+
+	public static List<Integer> riverSizes(int[][] matrix) {
+		// Write your code here.
+		return new ArrayList<Integer>();
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub

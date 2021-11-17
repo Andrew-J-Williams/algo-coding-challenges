197e22e9e388a17a3fcb363a31028261cc89edff
Andrew Williams <1williamsaj@gmail.com>
2021-11-17 08:20:31 -0600

    Added test data to kadanesAlgorithm

--- src/com/challenges/medium/KadanesAlgorithm.java
+++ src/com/challenges/medium/KadanesAlgorithm.java
@@ -8,8 +8,9 @@ public class KadanesAlgorithm {
 	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int [] array1 = { 3, 5, -9, 1, 3, -2, 3, 4, 7, 2, -9, 6, 3, 1, -5, 4 };
+		
+		System.out.println(kadanesAlgorithm(array1));
 	}
 
 }

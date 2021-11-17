7e15539b3851a647f383c66681fec3643f42eda6
Andrew Williams <1williamsaj@gmail.com>
2021-11-17 14:24:46 -0600

    Added solution for BreadthFirstSearch

--- src/com/challenges/medium/BreadthFirstSearch.java
+++ src/com/challenges/medium/BreadthFirstSearch.java
@@ -27,31 +27,38 @@ import java.util.*;
  */
 
 public class BreadthFirstSearch {
-	
+
 	static class Node {
-	    String name;
-	    List<Node> children = new ArrayList<Node>();
+		String name;
+		List<Node> children = new ArrayList<Node>();
 
-	    public Node(String name) {
-	      this.name = name;
-	    }
+		public Node(String name) {
+			this.name = name;
+		}
 
-	    public List<String> breadthFirstSearch(List<String> array) {
-	      // Write your code here.
-	      return array;
-	    }
+		public List<String> breadthFirstSearch(List<String> array) {
+			
+			Queue<Node> queue = new LinkedList<Node>();
+			queue.add(this);
+			
+			while(!queue.isEmpty()) {
+				Node current = queue.poll();
+				array.add(current.name);
+				queue.addAll(current.children);
+			}
+			
+			return array;
+		}
 
-	    public Node addChild(String name) {
-	      Node child = new Node(name);
-	      children.add(child);
-	      return this;
-	    }
-	  }
+		public Node addChild(String name) {
+			Node child = new Node(name);
+			children.add(child);
+			return this;
+		}
+	}
 
-	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		// See AlgoExpert
 	}
 
 }

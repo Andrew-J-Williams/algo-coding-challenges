3e1cd6e380375f00e5c00607d09dd9ba02ae3c0d
Andrew Williams <1williamsaj@gmail.com>
2021-11-18 15:39:28 -0600

    Added prompt code to RemoveIslands

--- src/com/challenges/medium/RemoveIslands.java
+++ src/com/challenges/medium/RemoveIslands.java
@@ -2,6 +2,11 @@ package com.challenges.medium;
 
 public class RemoveIslands {
 
+	public int[][] removeIslands(int[][] matrix) {
+		// Write your code here.
+		return new int[][] { {} };
+	}
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 

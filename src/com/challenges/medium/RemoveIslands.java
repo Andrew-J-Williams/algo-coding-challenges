67cb57084b0715fccc41003f75f565cb5120a9d9
Andrew Williams <1williamsaj@gmail.com>
2021-11-18 15:42:01 -0600

    Added test data to RemoveIslands

--- src/com/challenges/medium/RemoveIslands.java
+++ src/com/challenges/medium/RemoveIslands.java
@@ -2,14 +2,15 @@ package com.challenges.medium;
 
 public class RemoveIslands {
 
-	public int[][] removeIslands(int[][] matrix) {
+	public static int[][] removeIslands(int[][] matrix) {
 		// Write your code here.
 		return new int[][] { {} };
 	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int[][] matrix = { {1, 0, 0, 0, 0, 0}, {0, 1, 0, 1, 1, 1}, {0, 0, 1, 0, 1, 0}, {1, 1, 0, 0, 1, 0}, {1, 0, 1, 1, 0, 0}, {1, 0, 0, 0, 0, 1}};
 
+		System.out.println(removeIslands(matrix));
 	}
 
 }

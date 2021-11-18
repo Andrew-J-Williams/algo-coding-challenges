a3b32cc0108b4bdb274247454cfc4709541e4a76
Andrew Williams <1williamsaj@gmail.com>
2021-11-18 13:29:22 -0600

    Added prompt code to YoungestCommonAncestor

--- src/com/challenges/medium/YoungestCommonAncestor.java
+++ src/com/challenges/medium/YoungestCommonAncestor.java
@@ -2,6 +2,29 @@ package com.challenges.medium;
 
 public class YoungestCommonAncestor {
 
+	public static AncestralTree getYoungestCommonAncestor(AncestralTree topAncestor, AncestralTree descendantOne,
+			AncestralTree descendantTwo) {
+		// Write your code here.
+		return topAncestor; // Replace this line
+	}
+
+	static class AncestralTree {
+		public char name;
+		public AncestralTree ancestor;
+
+		AncestralTree(char name) {
+			this.name = name;
+			this.ancestor = null;
+		}
+
+		// This method is for testing only.
+		void addAsAncestor(AncestralTree[] descendants) {
+			for (AncestralTree descendant : descendants) {
+				descendant.ancestor = this;
+			}
+		}
+	}
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 

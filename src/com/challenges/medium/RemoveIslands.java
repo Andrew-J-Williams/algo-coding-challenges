24fe572f7a38561db67a40ef1a9a94c9ecd739d4
Andrew Williams <1williamsaj@gmail.com>
2021-11-20 12:21:23 -0600

    Minor comment change on RemoveIslands

--- src/com/challenges/medium/RemoveIslands.java
+++ src/com/challenges/medium/RemoveIslands.java
@@ -55,7 +55,8 @@ package com.challenges.medium;
 public class RemoveIslands {
 
 	public static int[][] removeIslands(int[][] matrix) {
-		// Write your code here.
+		
+		
 		return new int[][] { {} };
 	}
 

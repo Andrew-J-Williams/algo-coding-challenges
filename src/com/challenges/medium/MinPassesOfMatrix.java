b4d589467c28a43d9b4a8e70cc7b2c636e802400
Andrew Williams <1williamsaj@gmail.com>
2021-11-23 08:28:16 -0600

    Added prompt code to MinPassesOfMatrix

--- src/com/challenges/medium/MinPassesOfMatrix.java
+++ src/com/challenges/medium/MinPassesOfMatrix.java
@@ -2,6 +2,11 @@ package com.challenges.medium;
 
 public class MinPassesOfMatrix {
 
+	public int minimumPassesOfMatrix(int[][] matrix) {
+		// Write your code here.
+		return -1;
+	}
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 

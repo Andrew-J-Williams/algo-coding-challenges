ea647afeafa189ac351b43fcbfacd3efa76c6c2a
Andrew Williams <1williamsaj@gmail.com>
2021-11-23 08:31:10 -0600

    Added test data to MinPassesOfMatrix

--- src/com/challenges/medium/MinPassesOfMatrix.java
+++ src/com/challenges/medium/MinPassesOfMatrix.java
@@ -2,14 +2,15 @@ package com.challenges.medium;
 
 public class MinPassesOfMatrix {
 
-	public int minimumPassesOfMatrix(int[][] matrix) {
+	public static int minimumPassesOfMatrix(int[][] matrix) {
 		// Write your code here.
 		return -1;
 	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int[][] matrix = { {0, -1, -3, 2, 0}, {1, -2, -5, -1, -3}, {3, 0, 0, -4, -1}};
 
+		System.out.println(minimumPassesOfMatrix(matrix));
 	}
 
 }

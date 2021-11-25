382f49bfcdcf0fff27b72815dafcc7bf336471eb
Andrew Williams <1williamsaj@gmail.com>
2021-11-24 21:14:43 -0500

    Added prompt code to TaskAssignment

--- src/com/challenges/medium/TaskAssignment.java
+++ src/com/challenges/medium/TaskAssignment.java
@@ -1,6 +1,13 @@
 package com.challenges.medium;
 
+import java.util.*;
+
 public class TaskAssignment {
+
+	public ArrayList<ArrayList<Integer>> taskAssignment(int k, ArrayList<Integer> tasks) {
+		// Write your code here.
+		return new ArrayList<ArrayList<Integer>>();
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub

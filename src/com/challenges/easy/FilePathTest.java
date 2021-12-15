ed644797bc4e5bf030986cc2a9b49c8ecfe7dee7
Andrew Williams <1williamsaj@gmail.com>
2021-12-14 18:30:39 -0600

    Modified file for testing

--- src/com/challenges/easy/FilePathTest.java
+++ src/com/challenges/easy/FilePathTest.java
@@ -1,32 +1,42 @@
 package com.challenges.easy;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.Scanner;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+//import java.util.Scanner;
 
 public class FilePathTest {
 
 	public static void main(String[] args){
 
-		try {
-			File text = new File("test.txt");
-			System.out.println("Absolute Path: " + text.getAbsolutePath());
+		Path path = Paths.get("CLI Review.docx"); 
+		File text = new File("");
+		System.out.println(text);
+		
+		
+			System.out.println("Path Absolute Path: " + path.toAbsolutePath());
+			System.out.println("Absolute Path: " + text.getPath());
 			System.out.println("File Size: " + text.length());
 			System.out.println(" "); 
 		
-			Scanner scnr = new Scanner(text);
-		
-			int lineNumber = 1;
-		
-			while(scnr.hasNextLine()) {
-				String line = scnr.nextLine();
-				System.out.println("line " + lineNumber + ":" + line);
-				lineNumber++;
+			/*
+			FileReader fileStream = new FileReader(text);
+			
+			BufferedReader buffer = new BufferedReader(fileStream);
+			
+			String line = null;
+			
+			
+			while(	(line = buffer.readLine()) != null) {
+				System.out.println(line);
 			}
-		} catch (FileNotFoundException e) {
-			System.out.println("An error occurred.");
-			e.printStackTrace();
-		}
+			*/
+			
+		
 
 	}
 
